92b995e7a56426c37288d93e5d9370010c6d079f
Ross Crawford <sendrossemail@gmail.com>
2012-06-30 18:21:23 +0200

    step titles

--- glynt/apps/default/forms.py
+++ glynt/apps/default/forms.py
@@ -16,27 +16,33 @@ class AssassinStep2(forms.Form):
 # ----- WILLS -----
 
 class WillStep1(forms.Form):
+    step_title = forms.CharField(max_length=100, widget=forms.HiddenInput(attrs={'data-step-title':'A little bit about yourself'}))
     gender = forms.ChoiceField(label='Are you male or female?',help_text='You should select a Gender',choices=(('female','Female'),('male','Male')),initial='female',widget=forms.RadioSelect(attrs={'placeholder':'Currency','class':'md-updater','data-hb-name':'gender'}))
     partnership = forms.ChoiceField(label='Are you married or are you in a civil partnership?',help_text='Should defacto not be here?',choices=(('false','Not Married'),('married','Married'),('civil_partnership','Civil Partnership')),initial='false',widget=forms.RadioSelect(attrs={'placeholder':'Partnership Type','class':'md-updater','data-hb-name':'partnership'}))
     executor =  forms.ChoiceField(label='Will you be appointing an Executor for this will other than your Partner (if any)',choices=(('false','No Executor'),('appoint_executor','Appoint Executor')),initial='false',widget=forms.RadioSelect(attrs={'placeholder':'Executor','class':'md-updater','data-hb-name':'executor'}))
 
 class WillStep2(forms.Form):
+    step_title = forms.CharField(max_length=100, widget=forms.HiddenInput(attrs={'data-step-title':'Where do you live?'}))
     domicile = forms.ChoiceField(label='Where are you domiciled?',choices=(('england_wales','England and Wales'),('north_ireland','Northern Ireland')),widget=forms.RadioSelect(attrs={'placeholder':'Currency','class':'md-updater','data-hb-name':'domicile'}))
     partner_domicile = forms.ChoiceField(label='Where is your partner domiciled?',choices=(('england_wales','England and Wales'),('north_ireland','Northern Ireland')),widget=forms.RadioSelect(attrs={'placeholder':'Currency','class':'md-updater','data-hb-name':'partner_domicile'}))
 
 class WillStep3(forms.Form):
+    step_title = forms.CharField(max_length=100, widget=forms.HiddenInput(attrs={'data-step-title':'More Details'}))
     private_pension = forms.ChoiceField(label='Do you or your wife have a pension, other than a state pension, and/or a life assurance policy?',choices=(('yes','Yes'),('no','No')),widget=forms.RadioSelect(attrs={'placeholder':'Currency','class':'md-updater','data-hb-name':'private_pension'}))
     landowner = forms.ChoiceField(label='Do you and/or your wife own land or a property built on land (e.g. a house or flat)?',choices=(('yes','Yes'),('no','No')),widget=forms.RadioSelect(attrs={'placeholder':'Currency','class':'md-updater','data-hb-name':'landowner'}))
 
 class WillStep4(forms.Form):
+    step_title = forms.CharField(max_length=100, widget=forms.HiddenInput(attrs={'data-step-title':'Your name and stuff'}))
     full_name = forms.CharField(label='What is your full name?',max_length=100, widget=forms.TextInput(attrs={'placeholder':'Your Full Name','class':'md-updater','data-hb-name':'full_name'}))
     other_alias = forms.CharField(label='If you are known by any other name, enter it here.',max_length=100, widget=forms.TextInput(attrs={'placeholder':'Your Alias','class':'md-updater','data-hb-name':'other_alias'}))
     home_address = forms.CharField(label='What is your current home address?',widget=forms.Textarea(attrs={'placeholder':'Your Home Address','class':'md-updater','data-hb-name':'home_address'}))
 
 class WillStep5(forms.Form):
+    step_title = forms.CharField(max_length=100, widget=forms.HiddenInput(attrs={'data-step-title':'Partner Details'}))
     partner_full_name = forms.CharField(label='What is your partners full name?',max_length=100, widget=forms.TextInput(attrs={'placeholder':'Your Partner\'s Full Name','class':'md-updater','data-hb-name':'partner_full_name'}))
     partner_home_address = forms.CharField(label='What is your partners current home address?',widget=forms.Textarea(attrs={'placeholder':'Your Partner\'s Home Address','class':'md-updater','data-hb-name':'partner_home_address'}))
 
 class WillStep6(forms.Form):
+    step_title = forms.CharField(max_length=100, widget=forms.HiddenInput(attrs={'data-step-title':'Executor Details'}))
     executor_full_name = forms.CharField(label='What is full name of the executor you would like to appoint?',max_length=100, widget=forms.TextInput(attrs={'placeholder':'Full Name of Executor','class':'md-updater','data-hb-name':'executor_full_name'}))
     executor_address = forms.CharField(label='What is your partners current home address?',widget=forms.Textarea(attrs={'placeholder':'Address of the Executor','class':'md-updater','data-hb-name':'executor_address'}))
