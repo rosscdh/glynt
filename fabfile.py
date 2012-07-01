fe1dbe8cce895c820b0325e3bc984f5c8a800c5c
Ross Crawford <sendrossemail@gmail.com>
2012-07-01 08:12:30 +0200

    fab changes

--- fabfile.py
+++ fabfile.py
@@ -46,7 +46,7 @@ def deploy(hard_deploy, env, app_name, project_name, remote_project_path):
     with cd('%s/'%(remote_project_path,)):
       run('unzip /tmp/%s.zip'%(PROJECT,))
       cd( '%s/%s'%(remote_project_path, project_name,))
-      run('cp %s/%s/conf/%s/%s.local_settings.py %s/%s/%s/local_settings.py'%(remote_project_path,PROJECT,project_name,env, remote_project_path,PROJECT,app_name,))
+      # run('cp %s/%s/conf/%s/%s.local_settings.py %s/%s/%s/local_settings.py'%(remote_project_path,PROJECT,project_name,env, remote_project_path,PROJECT,app_name,))
       run('%s/apache2/bin/restart'%(remote_project_path,))
       # run('rm -Rf %s/%s/media'%(remote_project_path,project_name,))
       # run('rm -Rf %s/%s/static'%(remote_project_path,project_name,))
