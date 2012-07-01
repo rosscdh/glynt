9899c8e8c69e68ab5075c5d9edc08547c4805a7d
Ross Crawford <sendrossemail@gmail.com>
2012-07-01 08:20:45 +0200

    fab changes

--- conf/live.local_settings.py
+++ conf/live.local_settings.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+import os
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__+ '/../'))
+
+SITE_ID = 1
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+USE_ETAGS = True
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/stard0g101/webapps/cartvine_static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    '/home/stard0g101/webapps/cartvine_static/base/',
+)
