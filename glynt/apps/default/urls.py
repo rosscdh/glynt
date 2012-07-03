d7d2df89a71bc03febf680dee01544cb2b9ce999
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-07-03 16:27:03 +0200

    Added generic page

--- glynt/apps/default/urls.py
+++ glynt/apps/default/urls.py
@@ -8,5 +8,5 @@ from views import DocumentView, DocumentExportView
 urlpatterns = patterns('',
     url(r'^doc/(?P<doc>.+)/export/$', DocumentExportView.as_view(), name='export'),
     url(r'^doc/(?P<doc>.+)/$', DocumentView.as_view(template_name='default/document.html'), name='generic'),
-    url(r'^$', DocumentView.as_view(template_name='default/document.html'), name='default'),
+    url(r'^$', TemplateView.as_view(template_name='default/index.html'), name='default'),
 )
