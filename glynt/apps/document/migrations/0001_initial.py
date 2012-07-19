ba740ad434253de32f1b97655169fc380ab3cf36
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-07-19 08:14:31 -0700

    Added document migration

---
+++ glynt/apps/document/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Document'
+        db.create_table('document_document', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
+        ))
+        db.send_create_signal('document', ['Document'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Document'
+        db.delete_table('document_document')
+
+
+    models = {
+        'document.document': {
+            'Meta': {'object_name': 'Document'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['document']
