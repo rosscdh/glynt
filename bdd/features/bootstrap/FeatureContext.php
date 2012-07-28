44810df63062d9a2d993f8c67c01d0e6e4d371de
Ross Crawford <sendrossemail@gmail.com>
2012-07-28 18:07:24 +0200

    starting on bdd trying to get behat and mink playing nice

--- bdd/features/bootstrap/FeatureContext.php
+++ bdd/features/bootstrap/FeatureContext.php
@@ -27,10 +27,10 @@ class FeatureContext extends MinkContext
      *
      * @param array $parameters context parameters (set them up through behat.yml)
      */
-//    public function __construct(array $parameters)
-//    {
+    public function __construct(array $parameters)
+    {
         // Initialize your context here
-//    }
+    }
 
 //
 // Place your definition and hook methods here:
