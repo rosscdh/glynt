80e8d2bd92cca4ff28748a6c14fdbae13490c448
Ross Crawford <sendrossemail@gmail.com>
2012-07-29 15:08:07 +0200

    Default bddrun_tests saves as junit output for jenkins

--- bdd/run_tests.sh
+++ bdd/run_tests.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 source $WORKON_HOME/glynt/bin/activate
 
+# Kill previously running testserver_plus server
+kill -9 `ps -aef | grep "glynt/bin/python manage.py testserver" | grep -v grep | awk '{print $2}'`
+
 cd ../
 $WORKON_HOME/glynt/bin/python manage.py testserver &
 
 cd bdd/
-./bin/behat
+./bin/behat -f junit --out results
 
 # Kill the testserver_plus server
 kill -9 `ps -aef | grep "glynt/bin/python manage.py testserver" | grep -v grep | awk '{print $2}'`
