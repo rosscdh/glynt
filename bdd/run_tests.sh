2cada2f491d48ebb54080baae0eee6b9b7caaf52
Ross Crawford <sendrossemail@gmail.com>
2012-07-29 15:19:41 +0100

    getting tests working and saving reports in /reports dir

--- bdd/run_tests.sh
+++ bdd/run_tests.sh
@@ -9,7 +9,7 @@ echo "Kill Previous Servers"
 # Kill previously running testserver_plus server
 kill -9 `ps -aef | grep "glynt/bin/python manage.py testserver" | grep -v grep | awk '{print $2}'` > /dev/null 2>&1
 # Kill previously running sahi server
-kill -9 `ps -aef | grep "/usr/bin/java -classpath :../lib/sahi.jar:" | grep -v grep | awk '{print $2}'` > /dev/null 2>&1
+# kill -9 `ps -aef | grep "/usr/bin/java -classpath :../lib/sahi.jar:" | grep -v grep | awk '{print $2}'` > /dev/null 2>&1
 
 cd $PROJECT_DIR
 # Run the project unit tests
@@ -18,20 +18,19 @@ $WORKON_HOME/glynt/bin/python manage.py jenkins
 # Run the testserver so that behat can find and use it
 $WORKON_HOME/glynt/bin/python manage.py testserver &
 
-echo "Starting Sahi Server"
-cd $SAHI_DIR
-./sahi.sh &
+# echo "Starting Sahi Server"
+# cd $SAHI_DIR
+# ./sahi.sh &
 
 echo "Starting Behat Tests"
+echo $BDD_DIR
 cd $BDD_DIR
-./bin/behat -f junit --out results
+./bin/behat -f junit --out ../reports
 
 echo "Kill Servers"
 # Kill the testserver_plus server
 kill -9 `ps -aef | grep "glynt/bin/python manage.py testserver" | grep -v grep | awk '{print $2}'` > /dev/null 2>&1
 # Kill running sahi server
-kill -9 `ps -aef | grep "/usr/bin/java -classpath :../lib/sahi.jar:" | grep -v grep | awk '{print $2}'` > /dev/null 2>&1
-
-clear
+# kill -9 `ps -aef | grep "/usr/bin/java -classpath :../lib/sahi.jar:" | grep -v grep | awk '{print $2}'` > /dev/null 2>&1
 
 echo "Tests complete"
