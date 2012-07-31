59868a0c5fcef5b6d8b2090a853cc5aa009d8dd7
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-07-31 16:29:24 +0200

    added json_schema_validator and removed usless file

--- glynt/apps/flyform/forms.py
+++ glynt/apps/flyform/forms.py
@@ -7,6 +7,23 @@ from django.utils.translation import ugettext_lazy as _
 class BaseFlyForm(forms):
 	""" This form is the basis for the self generating form representations
 	it requires that a valid json_object be passed in which adheres to the following schema
+
+	schema = {
+		"type" : "object",
+		"properties" : {
+			"step_title" : {"type" : "string"},
+		    "field" : {"type" : "number"},
+			"widget" : {"type" : "string"},
+		    "name" : {"type" : "string"},
+		    "label" : {"type" : "string"},
+		    "required" : {"type" : "boolean"},
+		    "help_text" : {"type" : "string"},
+		    "placeholder" : {"type" : "string"},
+		    "class" : {"type" : "string"},
+		    "data-hb-name" : {"type" : "string"},
+		},
+	}
+
 	"""
 	def __init__(self, json_form_representation, *args, **kwargs):
 		super(BaseFlyForm, self).__init__(*args, **kwargs)
