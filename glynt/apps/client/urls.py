06b51829e8f93dff6bb134718d7e5278db651db1
Ross Crawford <sendrossemail@gmail.com>
2012-08-07 20:00:18 +0200

    Added anonymous_required decorator and applied to signup and login

--- glynt/apps/client/urls.py
+++ glynt/apps/client/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.views.decorators.csrf import csrf_exempt
 
 from views import SignupView, LoginView, DashboardView, HasLocalFacebookAccountView
+from glynt.decorators import anonymous_required
 
 
 urlpatterns = patterns('',
-    url(r'^signup/$', SignupView.as_view(), name='signup'),
-    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^signup/$', anonymous_required(SignupView.as_view()), name='signup'),
+    url(r'^login/$', anonymous_required(LoginView.as_view()), name='login'),
 
     url(r'^has/fb/account/$', HasLocalFacebookAccountView.as_view(), name='has_fb_account'),
 
