dbfee366bc847dce97caf56d13094c3de5dff223
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-08-08 08:20:04 -0700

    partially fixed tests need to fix document tests accounting for new flyform dict as string

--- glynt/apps/flyform/tests.py
+++ glynt/apps/flyform/tests.py
@@ -16,29 +16,30 @@ import inspect
 import re
 import copy
 
+BASE_JSON = {
+              "type": "step",
+              "properties": {
+                "step_title": "Step No. 1",
+                "hide_from": "",
+                "hide_to": "",
+                "iteration_title": "",
+              },
+              "fields": [
+                  {"field": "CharField",
+                  "widget": "TextInput",
+                  "name": "Test Field",
+                  "label": "Test",
+                  "required": "true",
+                  "help_text": "My Test Field",
+                  "placeholder": "tester",
+                  "class": "md-updater",
+                  "data-hb-name": "test_field"}
+              ]
+            }
 
 class BaseFlyFormTest(TestCase):
   def setUp(self):
-    self.base_json = {
-                      "type": "step",
-                      "properties": {
-                        "step_title": "Step No. 1",
-                        "hide_from": "",
-                        "hide_to": "",
-                        "iteration_title": "",
-                      },
-                      "fields": [
-                          {"field": "CharField",
-                          "widget": "TextInput",
-                          "name": "Test Field",
-                          "label": "Test",
-                          "required": "true",
-                          "help_text": "My Test Field",
-                          "placeholder": "tester",
-                          "class": "md-updater",
-                          "data-hb-name": "test_field"}
-                      ]
-                    }
+    self.base_json = json.dumps(BASE_JSON)
 
   def test_flyform_slugify(self):
     form = BaseFlyForm()
@@ -50,7 +51,7 @@ class BaseFlyFormTest(TestCase):
     self.assertEqual(form.slugify('test üöä 123'), u'test_uoa_123')
 
   def test_string_attribs(self):
-    form = BaseFlyForm(json.dumps(self.base_json))
+    form = BaseFlyForm(self.base_json)
     result = form.string_attribs({'my_key': 'my_value'})
     self.assertEqual(result, '[{"my_key": "my_value"}]')
 
@@ -58,18 +59,18 @@ class BaseFlyFormTest(TestCase):
     self.assertEqual(result, '[{"my_key2": "my_value2","my_key": "my_value"}]')
 
   def test_basic_step_form(self):
-    form = BaseFlyForm(json.dumps(self.base_json))
+    form = BaseFlyForm(self.base_json)
     self.assertEqual(type(form.fields['test_field']), fields.CharField)
     self.assertEqual(type(form.fields['test_field'].widget), widgets.TextInput)
     self.assertEqual(form.as_ul(), '<li><label for="id_test_field">Test:</label> <input name="test_field" id="id_test_field" placeholder="tester" type="text" class="md-updater" data-hb-name="test_field" /> <span class="helptext">My Test Field</span><input id="id_step_title" type="hidden" data-step-title="Step No. 1" name="step_title" /></li>')
 
   def test_setup_step_title(self):
-    form = BaseFlyForm(json.dumps(self.base_json))
+    form = BaseFlyForm(self.base_json)
     self.assertEqual(type(form.fields['step_title'].widget), widgets.HiddenInput)
     self.assertEqual(form.fields['step_title'].widget.attrs, {'data-step-title': u'Step No. 1'})
 
   def test_define_loopstep_attribs(self):
-    loop_step = self.base_json
+    loop_step = copy.deepcopy(BASE_JSON)
     loop_step['properties']['type'] = 'loop-step'
     loop_step['properties']['hide_from'] = 'Test Field'
     form = BaseFlyForm(json.dumps(loop_step))
@@ -96,15 +97,14 @@ class BaseFlyFormTest(TestCase):
           "data-hb-name": "test_field"}
       ]
     }
-    json_form_string = json.dumps(json_form)
-    form = BaseFlyForm(json_form_string)
+    form = BaseFlyForm(json.dumps(json_form))
 
     self.assertEqual(type(form.fields['test_field']), fields.CharField)
     self.assertEqual(type(form.fields['test_field'].widget), widgets.TextInput)
     self.assertEqual(form.as_ul(), '<li><label for="id_test_field">Test:</label> <input name="test_field" id="id_test_field" placeholder="tester" type="text" class="md-updater" data-hb-name="test_field" /> <span class="helptext">My Test Field</span><input id="id_step_title" type="hidden" data-step-title="Step No. 1" name="step_title" data-glynt-loop_step="[{"iteration_title": "Step No. 1","hide_from": "test_field"}]" /></li>')
 
   def test_basic_fields(self):
-    multi_field = self.base_json
+    multi_field = copy.deepcopy(BASE_JSON)
     multi_field['properties']['step_title'] = 'Multiple Step Test Form'
     for name in VALID_FIELD_TYPES:
       field = multi_field['fields'][0].copy()
@@ -121,7 +121,7 @@ class BaseFlyFormTest(TestCase):
 
   def test_multi_step_form(self):
     steps = []
-    step_set = self.base_json.copy()
+    step_set = copy.deepcopy(BASE_JSON)
     for index, name in enumerate(VALID_FIELD_TYPES):
       step = step_set.copy()
       step['properties']['step_title'] = 'Multiple Field Type Test Step %d' % (index+1,)
