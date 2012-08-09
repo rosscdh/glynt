96b195bb8db68c99fac38f337ef0008d4351a9c1
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-08-08 22:19:56 -0700

    started on avatar show process is working but need to automatically download remote images locally and process as thumbnail

--- glynt/apps/client/models.py
+++ glynt/apps/client/models.py
@@ -1,18 +1,22 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-
 from django.dispatch import receiver
-from socialregistration.signals import connect, profile_data
-
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from django.utils import safestring
 from jsonfield import JSONField
 
+from socialregistration.signals import connect, profile_data
 from userena.models import UserenaSignup, UserenaBaseProfile
 from userena.managers import ASSIGNED_PERMISSIONS
 from guardian.shortcuts import assign, get_perms
 
 from django_countries import CountryField
 from managers import GlyntUserManager as UserManager
+
+import urllib2
 
 
 class UserSignup(UserenaSignup):
@@ -30,6 +34,21 @@ class ClientProfile(UserenaBaseProfile):
   country = CountryField(default='US', null=True)
   state = models.CharField(max_length=64, null=True)
 
+  def get_mugshot_url(self):
+    """ Override the default """
+    # @TODO on save to thumbnail image; write a task that
+    # processes remote urls and downloads them locally
+    url = super(ClientProfile, self).get_mugshot_url()
+    validate = URLValidator(verify_exists=False)
+    tmp_url = urllib2.unquote(url.replace(settings.MEDIA_URL,''))
+    try:
+      validate(tmp_url)
+      # remove the static url from it
+      url = tmp_url
+    except ValidationError, e:
+      pass
+
+    return url
 
 @receiver(connect)
 def create_client_profile(sender, **kwargs):
@@ -52,5 +71,17 @@ def populate_profile_data(sender, **kwargs):
   profile, is_new = ClientProfile.objects.get_or_create(user=user)
   if 'profile_data' in kwargs:
     profile.profile_data = kwargs['profile_data']
+
+    # Handle facebook profile_photo
+    if 'profile_photo' in profile.profile_data:
+      # @TODO on save to thumbnail image; write a task that
+      # processes remote urls and downloads them locally
+      validate = URLValidator(verify_exists=True)
+      try:
+        validate(profile.profile_data['profile_photo'])
+        profile.mugshot = profile.profile_data['profile_photo']
+      except ValidationError, e:
+        pass
+
   profile.save()
 
