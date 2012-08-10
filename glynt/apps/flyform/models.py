99feb5830d2d21220910f230824a9ac2825c9610
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-08-10 07:54:43 -0700

    got document fixtures correct

--- glynt/apps/flyform/models.py
+++ glynt/apps/flyform/models.py
@@ -6,4 +6,6 @@ class FlyForm(models.Model):
     """ Flyform model used to store teh JSON representation of a form """
     body = JSONField(blank=False,null=False)
 
+    def __unicode__(self):
+      return u'FlyForm for %s' % (self.document.name,)
 
