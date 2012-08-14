1a6d38ce3f93184637bc1e03a5754b1b367bb7c1
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-08-13 22:26:08 -0700

    Added defaults field for populating document with default values

--- glynt/apps/flyform/models.py
+++ glynt/apps/flyform/models.py
@@ -7,9 +7,9 @@ from glynt.apps.document.models import Document
 class FlyForm(models.Model):
     """ Flyform model used to store teh JSON representation of a form """
     body = JSONField(blank=False,null=False)
+    defaults = JSONField(blank=True,null=True)
 
     def __unicode__(self):
-
       try:
         return u'FlyForm for %s' % (self.document.name,)
       except Document.DoesNotExist:
