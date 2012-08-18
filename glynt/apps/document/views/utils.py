f93c1fc63e7ce7efebffa9dd3b16c329ef3e1119
Ross Crawford <sendrossemail@gmail.com>
2012-08-18 17:40:16 +0200

    tests all green

--- glynt/apps/document/views/utils.py
+++ glynt/apps/document/views/utils.py
@@ -1,7 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import Http404, HttpResponse, HttpResponseBadRequest
 
 from glynt.apps.document.models import Document, ClientCreatedDocument
 
