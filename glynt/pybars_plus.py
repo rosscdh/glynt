07a13d70f7ba85e172a17c6a3b3eeebaec07122c
Ross Crawford <sendrossemail@gmail.com>
2012-08-26 17:50:15 +0200

    added primary pybars_plus need unit tests

--- glynt/pybars_plus.py
+++ glynt/pybars_plus.py
@@ -1,14 +1,70 @@
 from pybars import Compiler
 
-class PybarsPlus(object):
-  def __init__(self):
-    self.compiler = Compiler()
-    self.compiler.register_helper(u'if_eq', self._if_eq)
 
-  def _if_eq(self, this, *args, **kwargs):
+class PybarsPlus(object):
+  source = None
+  compiler = None
+  template = None
+
+  def __init__(self, source):
+    self.compiler = Compiler()
+    self.source = unicode(source)
+    self.compiler.register_helper(u'if_eq', self.helper_if_eq)
+
+  def render(self, context):
+    template = self.compiler.compile(self.source)
+    return ''.join(template(context))
+
+  def helper_if_eq(self, this, *args, **kwargs):
     options = args[0]
     if args[1] == kwargs['compare']:
       return options['fn'](this)
     else:
       return options['inverse'](this)
 
+  def helper_unless_eq(self, this, *args, **kwargs):
+    options = args[0]
+    if args[1] != kwargs['compare']:
+      return options['fn'](this)
+    else:
+      return options['inverse'](this)
+
+  def helper_if_gt(self, this, *args, **kwargs):
+    options = args[0]
+    if args[1] > kwargs['compare']:
+      return options['fn'](this)
+    else:
+      return options['inverse'](this)
+
+  def helper_unless_gt(self, this, *args, **kwargs):
+    pass
+
+  def helper_if_lt(self, this, *args, **kwargs):
+    options = args[0]
+    if args[1] < kwargs['compare']:
+      return options['fn'](this)
+    else:
+      return options['inverse'](this)
+
+  def helper_unless_lt(self, this, *args, **kwargs):
+    pass
+
+  def helper_if_gteq(self, this, *args, **kwargs):
+    options = args[0]
+    if args[1] >= kwargs['compare']:
+      return options['fn'](this)
+    else:
+      return options['inverse'](this)
+
+  def helper_unless_gteq(self, this, *args, **kwargs):
+    pass
+
+  def helper_if_lteq(self, this, *args, **kwargs):
+    options = args[0]
+    if args[1] <= kwargs['compare']:
+      return options['fn'](this)
+    else:
+      return options['inverse'](this)
+
+  def helper_unless_lteq(self, this, *args, **kwargs):
+    pass
