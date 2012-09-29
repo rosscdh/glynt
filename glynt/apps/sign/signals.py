8bda59a4ef70f7cbbd608e4f9a21011de5efe1c3
Ross Crawford <sendrossemail@gmail.com>
2012-09-29 12:22:18 +0200

    getting signatures working with more detail, fixed unicode to str dict conversion error for splats

--- glynt/apps/sign/signals.py
+++ glynt/apps/sign/signals.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 from glynt.apps.sign.models import DocumentSignature
 from glynt.apps.sign.tasks import send_signature_invite_email, send_signature_acquired_email
+
 import datetime
 
 
@@ -16,16 +17,17 @@ def save_document_signature_signal(sender, **kwargs):
     signature = kwargs['instance']
     document = signature.document
 
+    meta_data = dict([(str(k), v) for k, v in signature.meta_data.items()])
     if signature.is_signed == True:
         document.increment_num_signed(signature.id)
         try:
-          send_signature_acquired_email.delay(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **signature.meta_data)
+          send_signature_acquired_email.delay(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **meta_data)
         except:
-          send_signature_acquired_email(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **signature.meta_data)
+          send_signature_acquired_email(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **meta_data)
 
     else:
         document.increment_num_invited(signature.id)
         try:
-          send_signature_invite_email.delay(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **signature.meta_data)
+          send_signature_invite_email.delay(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **meta_data)
         except:
-          send_signature_invite_email(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **signature.meta_data)
+          send_signature_invite_email(document=signature.document, date_invited=signature.date_invited, key_hash=signature.key_hash, **meta_data)
