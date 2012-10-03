5b461f835dc6accf36fe2693b72a8bd8583307fa
Ross Crawford <sendrossemail@gmail.com>
2012-10-03 11:19:27 +0200

    made teh review page and the invite to sign page sammified

--- glynt/apps/document/templatetags/document_tags.py
+++ glynt/apps/document/templatetags/document_tags.py
@@ -9,3 +9,15 @@ def document_categories_home():
     return {
     'objects': DocumentCategory.objects.filter(active=True)
     }
+
+
+@register.inclusion_tag('document/comments/base.html')
+def show_comments_for_object(obj, next):
+    return {
+    'object': obj,
+    'next': next
+    }
+
+@register.inclusion_tag('document/comments/comments.js')
+def comments_js():
+    return {}
