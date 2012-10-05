640090a2b50a2ef3f5fefae29004495bc381c882
Ross Crawford <sendrossemail@gmail.com>
2012-10-05 19:52:21 +0200

    progress

--- glynt/apps/document/forms.py
+++ glynt/apps/document/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from bootstrap.forms import BootstrapMixin, Fieldset
 
 from glynt.apps.document.models import ClientCreatedDocument
 
@@ -17,3 +18,26 @@ class ClientCreatedDocumentForm(forms.ModelForm):
   class Meta:
     model = ClientCreatedDocument
     fields = ('id', 'name')
+
+
+class CreateStepForm(forms.Form, BootstrapMixin):
+    """ The template form used to help the authoring tool """
+    STEP_TYPES = (
+        ('step', 'Normal Step'),
+        ('loop-step', 'Loop Step')
+    )
+    step_type = forms.ChoiceField(choices=STEP_TYPES)
+    
+    class Meta:
+        layout = (
+            Fieldset("Login Details", "email", "password1", "password2"),
+        )
+
+
+class CreateStepFieldForm(forms.Form, BootstrapMixin):
+    """ The template form used to help the authoring tool """
+
+    class Meta:
+        layout = (
+            Fieldset("Login Details", "email", "password1", "password2"),
+        )
