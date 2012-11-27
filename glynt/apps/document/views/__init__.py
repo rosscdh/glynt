88ea6bdd1975e53583b593e98defbf5eb80e12f9
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-11-26 22:08:38 -0800

    readded views

---
+++ glynt/apps/document/views/__init__.py
@@ -0,0 +1,4 @@
+from glynt.apps.document.views.category import *
+from glynt.apps.document.views.utils import *
+from glynt.apps.document.views.document import *
+from glynt.apps.document.views.client_document import *
