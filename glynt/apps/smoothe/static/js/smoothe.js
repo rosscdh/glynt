6275eb8ce68456180646cbecd22f059a1a8bcc10
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-11-28 10:17:17 -0800

    progress on handlebars template handlers

--- glynt/apps/smoothe/static/js/smoothe.js
+++ glynt/apps/smoothe/static/js/smoothe.js
@@ -20,31 +20,27 @@
 //     self.init(params)
 // };
 
-Handlebars.registerHelper('doc_var', function(context, options) {
+Handlebars.registerHelper('doc_var', function(options) {
 
-    if (context.hash.name === undefined || context.hash.name === '') {
-        throw 'doc_var requires a unique "name"';
+    if (options.hash.name === undefined || options.hash.name === '') {
+        throw new Error('doc_var requires a unique "name"');
     }
-    var app = (context.hash.app === undefined) ? window.app : eval('window.'.format(context.hash.app)) ;
-    var var_name = context.hash.name;
+    var app = (options.hash.app === undefined) ? window.app : eval('window.'.format(options.hash.app)) ;
+    var var_name = options.hash.name;
     var field_type = 'text';
-    var has_initial = null;
+    var has_initial = (options.hash.initial === undefined) ? false : true;
     var value = null;
     var html_return = '<span class="{type} edit" data-has_initial="{has_initial}" data-doc_var="{variable_name}">{value}</span>';
 
-    if (context.hash.field_type !== undefined && context.hash.field_type === '') {
-        field_type = context.hash.field_type;
+    if (options.hash.field_type !== undefined && options.hash.field_type === '') {
+        field_type = options.hash.field_type;
     }
-    // see if this context.name is already defined in the app context (to get user populated data)
-    try{
-        // context.hash.initial = context.call(this);
-    }catch(e){
-        // as we set the hash initial based on html_inner above we need do nothing
-        // except catch the error
+    // see if this options.name is already defined in the app context (to get user populated data)
+    if (has_initial === false) {
+        options.hash.initial = options.fn(this);
     }
 
-    has_initial = (context.hash.initial === undefined) ? false : true;
-    value = (context.hash.initial !== undefined) ? context.hash.initial : '_____'.repeat(2) ;
+    value = (options.hash.initial !== undefined) ? options.hash.initial : '' ;
     value = (app.context[var_name] === undefined) ? value : app.context[var_name] ;
 
     // add value to context
@@ -55,50 +51,59 @@ Handlebars.registerHelper('doc_var', function(context, options) {
     // wrap the value in our detailed html to allow UX interaction
     html_return = html_return.assign({ 'variable_name': var_name, 'value': value, 'type': 'doc_var', 'has_initial': has_initial });
 
-    context.hash.field_type = field_type;
-    context.hash.html_return = html_return;
+    options.hash.field_type = field_type;
+    options.hash.html_return = html_return;
 
     // set the context
-    app.helper_context[var_name] = context.hash;
+    app.helper_context[var_name] = options.hash;
 
     // make it safe so hb does not mess with it
     return new Handlebars.SafeString(html_return);
 });
 
 
-Handlebars.registerHelper('doc_choice', function(context, options) {
-    console.log(context)
-    if (context.hash.name === undefined || context.hash.name === '') {
-        throw 'doc_choice requires a unique "name"';
+Handlebars.registerHelper('doc_choice', function(options) {
+    if (options.hash.name === undefined || options.hash.name === '') {
+        throw new Error('doc_choice requires a unique "name"');
     }
-    var app = (context.hash.app === undefined) ? window.app : eval('window.'.format(context.hash.app)) ;
-    var var_name = context.hash.name;
-    var choices = context.hash.choices;
-    console.log(this.name)
+    var app = (options.hash.app === undefined) ? window.app : eval('window.'.format(options.hash.app)) ;
+    var var_name = options.hash.name;
+    var choices = options.hash.choices;
     var html_return = '';
-
+console.log(options.hash.choices)
     // set the context
-    app.helper_context[var_name] = context.hash;
+    app.helper_context[var_name] = options.hash;
 
     // make it safe so hb does not mess with it
     return new Handlebars.SafeString(html_return);
 });
 
 
-Handlebars.registerHelper('doc_select', function(context, options) {
-    if (context.hash.name === undefined || context.hash.name === '') {
-        throw 'doc_select requires a unique "name"';
+Handlebars.registerHelper('doc_select', function(options) {
+    if (options.hash.name === undefined || options.hash.name === '') {
+        throw new Error('doc_select requires a unique "name"');
     }
-    var app = (context.hash.app === undefined) ? window.app : eval('window.'.format(context.hash.app)) ;
-    var var_name = context.hash.name;
-    //var content = context.call(this);
-    //content = content.split('{option}');// splti by the {option} seperator
-
+    var app = (options.hash.app === undefined) ? window.app : eval('window.'.format(options.hash.app)) ;
+    var var_name = options.hash.name;
+    var title = options.hash.name;
+    var can_toggle = (options.hash.can_toggle === undefined) ? false: options.hash.can_toggle;
     var html_return = '';
+    var content = options.fn(this);
+    options.hash.select_options = [];
+    select_options = content.split('{option}');// splti by the {option} seperator
+    for (var i = 0; i < select_options.length; i++) {
+        options.hash.select_options.push({
+            'text': select_options[i],
+            'selected': false,
+            'index': i
+        });
+    }
+
+    html_return = Handlebars.compile('<ul id="{{id}}" data-can_toggle="{{can_toggle}}" data-multi="{{multi}}" class="doc_select {{class}}">{{#each select_options}}<li data-option_index="{{index}}" class="{{#if selected}}selected{{/if}}">{{text}}</li>{{/each}}</ul>');
 
     // set the context
-    app.helper_context[var_name] = context.hash;
+    app.helper_context[var_name] = options.hash;
 
     // make it safe so hb does not mess with it
-    return new Handlebars.SafeString(html_return);
+    return html_return(options.hash);
 });
