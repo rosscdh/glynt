4a3ce548000f634b6e4c855f3147f51ca0f640bc
Ross Crawford <sendrossemail@gmail.com>
2013-01-14 19:32:19 +0100

    removedelasticsearch from api

--- glynt/apps/api/v1.py
+++ glynt/apps/api/v1.py
@@ -1,8 +1,6 @@
 import ast
 from django.conf import settings
 
-from tastypie_elasticsearch.resources import ElasticSearch
-
 from tastypie import fields, utils
 from tastypie.resources import ModelResource
 from tastypie.api import Api
@@ -80,25 +78,7 @@ class SignatureResource(BaseApiModelResource):
         return bundle
 
 
-class BaseElasticSearchResource(ElasticSearch):
-    class Meta:
-        authentication = BasicAuthentication()
-        authorization = Authorization()
-        serializer = Serializer(formats=available_formats)
-        es_server = getattr(settings, "ES_INDEX_SERVER", "http://127.0.0.1:9200/")
-        es_timeout = 20
-
-
-class UserTagsResource(BaseElasticSearchResource):
-    user = fields.IntegerField(attribute='user', null=False)
-    tag = fields.CharField(attribute='tag', null=False)
-    class Meta(BaseElasticSearchResource.Meta):
-        indices = ["user_tags"]
-        resource_name = 'client/tags'
-
-
 """ Register the api resources """
 v1_internal_api.register(DocumentResource())
 v1_internal_api.register(ClientCreatedDocumentResource())
 v1_internal_api.register(SignatureResource())
-v1_internal_api.register(UserTagsResource())
