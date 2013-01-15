1021412849f1f152a22696d716679743b83fbef6
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-01-14 21:46:07 -0800

    integrating behave test suite

--- glynt/test_runner.py
+++ glynt/test_runner.py
@@ -1,9 +1,8 @@
 # Make our own testrunner that by default only tests our own apps
 from django.conf import settings
-from django.test.simple import DjangoTestSuiteRunner
+from django_behave.runner import DjangoBehaveTestSuiteRunner
 
-
-class GlyntAppTestRunner(DjangoTestSuiteRunner):
+class GlyntAppTestRunner(DjangoBehaveTestSuiteRunner):
     def build_suite(self, test_labels, *args, **kwargs):
         PROJECT_APPS = []
         # Remove path info and use only the app "label"
