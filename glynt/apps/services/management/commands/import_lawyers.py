a00be7b47b2b1c0dfc1be5387108bfdd0d97f3ae
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-03-27 18:39:36 +0100

    lawyer import working

--- glynt/apps/services/management/commands/import_lawyers.py
+++ glynt/apps/services/management/commands/import_lawyers.py
@@ -10,7 +10,7 @@ from glynt.apps.lawyer.models import Lawyer
 from glynt.apps.firm.models import Firm, Office
 
 import csv
-
+import pdb
 
 class Command(BaseCommand):
     """ Imports Lawyers from the GDrive LawyerDB spreadsheet
@@ -69,18 +69,26 @@ class Command(BaseCommand):
                 if i > 0:
                     username = slugify(u'%s-%s'% (r[1], r[2],))
                     try:
-                        u, is_new = User.objects.get_or_create(username=username, first_name=r[1], last_name=r[2], email=r[6])
+                        u, user_is_new = User.objects.get_or_create(username=username, first_name=r[1], last_name=r[2], email=r[6])
                     except IntegrityError:
                         username = slugify(u'%s-%s%s'% (r[1], r[2],r[6],))
-                        u, is_new = User.objects.get_or_create(username=username, first_name=r[1], last_name=r[2], email=r[6])
+                        u, user_is_new = User.objects.get_or_create(username=username, first_name=r[1], last_name=r[2], email=r[6])
                     print u
-                    data = {'twitter':r[11], 'angel_list':r[5], 'linkedin':r[9], 'facebook':r[10], 'location':r[7], 'phone':r[8]}
-                    role = Lawyer.LAWYER_ROLES.get_value_by_name(r[4].lower())
-                    l, lawyer_is_new = Lawyer.objects.get_or_create(user=u, bio=r[len(r)-1], role=role, data=data)
-                    print l
-                    f, is_new = Firm.objects.get_or_create(name=r[3])
-                    if lawyer_is_new:
-                        f.lawyers.add(u)
+                    if user_is_new:
+                        data = {'twitter':r[11], 'angel_list':r[5], 'linkedin':r[9], 'facebook':r[10], 'location':r[7], 'phone':r[8]}
+                        role = Lawyer.LAWYER_ROLES.get_value_by_name(r[4].lower())
+                        l, lawyer_is_new = Lawyer.objects.get_or_create(user=u, bio=r[len(r)-1], role=role, data=data)
+                        print l
+                        f, firm_is_new = Firm.objects.get_or_create(name=r[3])
+                        if lawyer_is_new:
+                            f.lawyers.add(u)
+
+                        try:
+                            f.office
+                        except Office.DoesNotExist:
+                            # add office
+                            data = {'phone':r[8]}
+                            Office.objects.get_or_create(firm=f, address=r[7], data=data)
                         print "added %s to %s" % (l,f,)
 >>>>>>> 77f00d1... working on importer
 
