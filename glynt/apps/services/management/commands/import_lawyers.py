709fc9145af90e56843d4752dd2745f295b4d231
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-03-27 18:40:57 +0100

    comments

--- glynt/apps/services/management/commands/import_lawyers.py
+++ glynt/apps/services/management/commands/import_lawyers.py
@@ -39,7 +39,6 @@ class Command(BaseCommand):
             dialect = csv.Sniffer().sniff(csv_file.read(1024))
             csv_file.seek(0)
             for i,r in enumerate(csv.reader(csv_file, dialect)):
-<<<<<<< HEAD
                 # skip title row
                 if i > 0:
                     username = slugify(u'%s-%s'% (r[1], r[2],))
@@ -65,7 +64,8 @@ class Command(BaseCommand):
                             data = {'phone':r[8]}
                             Office.objects.get_or_create(firm=f, address=r[7], data=data)
                     print "added %s to %s" % (l,f,)
-=======
+
+                # skip title row
                 if i > 0:
                     username = slugify(u'%s-%s'% (r[1], r[2],))
                     try:
@@ -73,12 +73,12 @@ class Command(BaseCommand):
                     except IntegrityError:
                         username = slugify(u'%s-%s%s'% (r[1], r[2],r[6],))
                         u, user_is_new = User.objects.get_or_create(username=username, first_name=r[1], last_name=r[2], email=r[6])
-                    print u
+
                     if user_is_new:
                         data = {'twitter':r[11], 'angel_list':r[5], 'linkedin':r[9], 'facebook':r[10], 'location':r[7], 'phone':r[8]}
                         role = Lawyer.LAWYER_ROLES.get_value_by_name(r[4].lower())
                         l, lawyer_is_new = Lawyer.objects.get_or_create(user=u, bio=r[len(r)-1], role=role, data=data)
-                        print l
+
                         f, firm_is_new = Firm.objects.get_or_create(name=r[3])
                         if lawyer_is_new:
                             f.lawyers.add(u)
@@ -89,6 +89,6 @@ class Command(BaseCommand):
                             # add office
                             data = {'phone':r[8]}
                             Office.objects.get_or_create(firm=f, address=r[7], data=data)
-                        print "added %s to %s" % (l,f,)
->>>>>>> 77f00d1... working on importer
+
+                    print "added %s to %s" % (l,f,)
 
