f6f6db1132b0f04781d0de4d1eb00d13cd22e52b
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-03-27 19:01:59 +0100

    inor fixes

--- glynt/apps/services/management/commands/import_lawyers.py
+++ glynt/apps/services/management/commands/import_lawyers.py
@@ -52,43 +52,20 @@ class Command(BaseCommand):
                         data = {'twitter':r[11], 'angel_list':r[5], 'linkedin':r[9], 'facebook':r[10], 'location':r[7], 'phone':r[8]}
                         role = Lawyer.LAWYER_ROLES.get_value_by_name(r[4].lower())
                         l, lawyer_is_new = Lawyer.objects.get_or_create(user=u, bio=r[len(r)-1], role=role, data=data)
+                        print "lawyer: %s" % l
 
                         f, firm_is_new = Firm.objects.get_or_create(name=r[3])
+                        print "firm: %s" % f
+
                         if lawyer_is_new:
                             f.lawyers.add(u)
+                            print "joined %s with firm: %s" % (l,f,)
 
                         try:
                             f.office
                         except Office.DoesNotExist:
                             # add office
                             data = {'phone':r[8]}
-                            Office.objects.get_or_create(firm=f, address=r[7], data=data)
-                    print "added %s to %s" % (l,f,)
-
-                # skip title row
-                if i > 0:
-                    username = slugify(u'%s-%s'% (r[1], r[2],))
-                    try:
-                        u, user_is_new = User.objects.get_or_create(username=username, first_name=r[1], last_name=r[2], email=r[6])
-                    except IntegrityError:
-                        username = slugify(u'%s-%s%s'% (r[1], r[2],r[6],))
-                        u, user_is_new = User.objects.get_or_create(username=username, first_name=r[1], last_name=r[2], email=r[6])
-
-                    if user_is_new:
-                        data = {'twitter':r[11], 'angel_list':r[5], 'linkedin':r[9], 'facebook':r[10], 'location':r[7], 'phone':r[8]}
-                        role = Lawyer.LAWYER_ROLES.get_value_by_name(r[4].lower())
-                        l, lawyer_is_new = Lawyer.objects.get_or_create(user=u, bio=r[len(r)-1], role=role, data=data)
-
-                        f, firm_is_new = Firm.objects.get_or_create(name=r[3])
-                        if lawyer_is_new:
-                            f.lawyers.add(u)
-
-                        try:
-                            f.office
-                        except Office.DoesNotExist:
-                            # add office
-                            data = {'phone':r[8]}
-                            Office.objects.get_or_create(firm=f, address=r[7], data=data)
-
-                    print "added %s to %s" % (l,f,)
+                            o, office_is_new = Office.objects.get_or_create(firm=f, address=r[7], data=data)
+                            print "added office: %s" % o
 
