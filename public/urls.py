7f6b5d340e1cea1a911399b78950b95975d0d6e4
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-04-02 19:09:56 +0200

    started on base redirect logic for lawyer startup investor login

--- public/urls.py
+++ public/urls.py
@@ -6,11 +6,16 @@ from django.views.generic.edit import ProcessFormView
 from glynt.apps.lawyer.forms import LawyerProfileSetupForm
 from glynt.apps.lawyer.views import LawyerProfileSetupView
 
+from views import LoggedInRedirectView
+
+
 urlpatterns = patterns('',
     # T&C
     url(r'^terms/$', TemplateView.as_view(template_name='public/terms-and-conditions.html'), name='terms'),
+    # Logged-in
+    url(r'^logged-in/$', LoggedInRedirectView.as_view(), name='logged_in_generic'),
     # lawyers
-    url(r'^lawyers/setup/profile/$', login_required(LawyerProfileSetupView.as_view()), name='lawyer_setup_profile'),
+    url(r'^lawyers/profile/setup/$', login_required(LawyerProfileSetupView.as_view()), name='lawyer_setup_profile'),
     url(r'^lawyers/$', login_required(TemplateView.as_view(template_name='public/lawyer-welcome.html')), name='lawyer'),
     # home
     url(r'^$', TemplateView.as_view(template_name='public/homepage.html'), name='homepage'),
