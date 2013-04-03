339cad773387c1adc28fd2caaaebde766874ff99
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-04-03 07:25:56 +0200

    removed invalid check for static urls

--- glynt/urls.py
+++ glynt/urls.py
@@ -40,6 +40,6 @@ urlpatterns = patterns('',
 )
 
 
-if settings.DEBUG or settings.LOCAL_SETTINGS is True:
+if settings.DEBUG:
 	urlpatterns += staticfiles_urlpatterns()
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
