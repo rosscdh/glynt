1d11d106267221b59471f063a5b809c1d3653997
Michael Pedersen <mp@beefy.dk>
2013-04-04 22:40:26 +0200

    Implements angel list fetching based on tags

--- glynt/apps/startups/management/commands/import_startups.py
+++ glynt/apps/startups/management/commands/import_startups.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 import time
 import re
 from optparse import make_option
@@ -8,11 +7,6 @@ import requests
 import lxml.html
 
 from django.core.management.base import BaseCommand
-from django.template.defaultfilters import slugify
-from django.utils.encoding import smart_unicode
-
-
-RE_500URL= re.compile(r'http://500\.co/startup-profiles/.+/')
 
 
 class Command(BaseCommand):
@@ -28,7 +22,47 @@ class Command(BaseCommand):
     #local_file = None
 
     def handle(self, *args, **options):
-        print self.fetch_500startups()
+        pass
+
+    # angellist
+
+    def fetch_angellist(self):
+        """ Fetches startups from angel.co
+
+        They are pretty explicit about not using their api for scraping,
+        so we'll just take a small sample (a couple hundred companies or so)
+
+        Their tags are in a tree-structure starting from
+            Market: All Markets (9217)
+            Location: Earth (1643)
+
+        If we select startups starting at these points, the assumption is
+        that we should hit every company in their database.
+
+        However, for development data, we will concentrate on the
+        following tag:
+            Location: Earth: North America: United States (1688)
+        """
+        startups = self.get_angellist_startups_for_tag(1688, limit=100)
+        return startups
+
+    def get_angellist_startups_for_tag(self, tag_id, limit=None):
+        def get_page(page):
+            return requests.get('https://api.angel.co/1/tags/%s/startups' % tag_id, params={'page': page}).json()
+
+        page = 1
+        results = []
+        while True:
+            response = get_page(page)
+            results.extend([s for s in response['startups'] if s.get('hidden') is False])
+
+            if response['page'] >= response['last_page'] or (limit and len(results) >= limit):
+                return results[:limit]
+
+            time.sleep(4)  # angellist has a req/hour limit of 1000 or about one per 3.75 sec
+            page += 1
+
+    # 500startups
 
     def fetch_500startups(self):
         """ Fetches startups from 500.co/startups.
@@ -37,7 +71,7 @@ class Command(BaseCommand):
         # normally i would use lxml, but their html is horribly broken
         # not even beautifulsoup can handle it
         r = requests.get('http://500.co/startups/')
-        urls = list(set(RE_500URL.findall(r.text)))
+        urls = list(set(re.findall(r'http://500\.co/startup-profiles/.+/', r.text)))
 
         # fetch each indidual startup
         results = []
