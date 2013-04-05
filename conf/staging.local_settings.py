2d9bb77f792b2b125dcb0d3558d1966d31e6e620
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-04-05 18:34:38 +0200

    added staging conf

---
+++ conf/staging.local_settings.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+LOCAL_SETTINGS = True
+from settings import *
+
+PROJECT_ENVIRONMENT = 'prod'
+
+SITE_ID = 3
+
+DEBUG = True
+COMPRESS_ENABLED = False
+COMPRESS_OFFLINE = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'stard0g101_glynt',
+        'USER': 'stard0g101_glynt',
+        'PASSWORD': '1003507b',
+        'HOST': 'web48.webfaction.com',
+        # 'PORT': '',
+    }
+}
+
+TIME_ZONE = 'Europe/London'
+
+USE_ETAGS = True
+
+MEDIA_URL = '/static/media/'
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/stard0g101/webapps/glynt_static/'
+MEDIA_ROOT = '/home/stard0g101/webapps/glynt_static/media/'
+
+
+FACEBOOK_API_KEY = '343632075713954'
+FACEBOOK_SECRET_KEY = '4f9854b8fe8f5ccf27ac1ffcf5051b79'
+
+LINKEDIN_CONSUMER_KEY = 'gnesv6zvhzgn'
+LINKEDIN_CONSUMER_SECRET = '3eTYERhJZd4UJSjM'
+LINKEDIN_SCOPE = ['r_basicprofile', 'r_emailaddress']
+LINKEDIN_EXTRA_FIELD_SELECTORS = ['email-address', 'headline', 'industry']
+LINKEDIN_EXTRA_DATA = [('id', 'id'),
+                       ('first-name', 'first_name'),
+                       ('last-name', 'last_name'),
+                       ('email-address', 'email_address'),
+                       ('headline', 'headline'),
+                       ('industry', 'industry')]
+
+
+# ANGEL_CLIENT_ID = '06aa0b726a71dc994bb44c3c4f3d9b91' # www.lawpal.com
+# ANGEL_CLIENT_SECRET = '26ea24f6107df875d2a410a8e2f55a27' # www.lawpal.com
+ANGEL_CLIENT_ID = '77339d6d557a0ae6c835baf89a22c2b0' # dev.lawpal.com
+ANGEL_CLIENT_SECRET = '8048545e591af07e18eef3049667decb' # dev.lawpal.com
+ANGEL_AUTH_EXTRA_ARGUMENTS = {'scope': 'email'}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'lawpal'
+EMAIL_HOST_PASSWORD = '0113633alex'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'glynt@dev.weareml.com'
+SERVER_EMAIL = 'glynt@dev.weareml.com'
+
+HELLOSIGN_AUTH = ("sendrossemail@gmail.com", "zanshin77")
+
+ALLOWED_HOSTS = ['dev.lawpal.com']
+
+RAVEN_CONFIG = {
+    'dsn': 'https://b5a6429d03e2418cbe71cd5a4c9faca6:ddabb51af47546d1ac0e63cb453797ba@app.getsentry.com/6287',
+}
+
+SPLUNKSTORM_ENDPOINT = 'logs2.splunkstorm.com'
+SPLUNKSTORM_PORT = 20824
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'unix:/home/stard0g101/memcached.sock'
+    },
+    'fallback': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/tmp/glynt.cache',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'splunkstorm':{
+            'level': 'INFO',
+            'class': 'glynt.logger.SplunkStormLogger',
+            'formatter': 'verbose'
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['splunkstorm'],
+            'level': 'INFO',
+            'propagate': False,
+        }
+    }
+}
