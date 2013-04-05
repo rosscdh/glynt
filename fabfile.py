d7a3b92af35fe2a206361ea11179948bba3e9c4c
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-04-05 11:21:56 +0200

    almost working prod

--- fabfile.py
+++ fabfile.py
@@ -1,51 +1,43 @@
 from __future__ import with_statement
+import os
 from fabric.api import *
 from fabric.contrib.console import confirm
 from fabric.contrib import files
-from time import gmtime, strftime
+
+import datetime
+import time
 
 debug = True
 
-PROJECT = 'glynt'
-
-PROJECT_PATH = '/home/rossc/Projects/Personal/RuleNumberOne/%s' % (PROJECT,)
-
-
-PROJECT_DEPLOY_INSTANCE = (('glynt', 'glynt'),)
-
-REMOTE_PROJECT_PATHS = []
-for app, project in PROJECT_DEPLOY_INSTANCE:
-  REMOTE_PROJECT_PATHS.append( (app, project, '/home/stard0g101/webapps/%s' % (project,)) )
-
-HOSTS = {
-    'staging': [],
-    'production': ['stard0g101@stard0g101.webfactional.com']
-}
-
-env.user = 'stard0g101'
 env.SHA1_FILENAME = None
-env.DEPLOY_ARCHIVE_PATH = '~/'
 
-FILENAME_TIMESTAMP = strftime("%m-%d-%Y-%H:%M:%S", gmtime())
+env.timestamp = time.time()
+
 
 @task
 def production():
     env.project = 'glynt'
     env.environment = 'production'
-    env.project_path = '/var/apps/lawpal/%s' % env.project
+    env.local_project_path = os.path.dirname(os.path.realpath(__file__))
+    env.remote_project_path = '/var/apps/lawpal/'
+    env.deploy_archive_path = '~/'
 
     # change from the default user to 'vagrant'
     env.user = 'ubuntu'
     # connect to the port-forwarded ssh
-    env.hosts = ['ec2-204-236-152-5.us-west-1.compute.amazonaws.com', 'ec2-184-72-21-48.us-west-1.compute.amazonaws.com']
+    #env.hosts = ['ec2-204-236-152-5.us-west-1.compute.amazonaws.com', 'ec2-184-72-21-48.us-west-1.compute.amazonaws.com']
+    env.hosts = ['ec2-184-72-21-48.us-west-1.compute.amazonaws.com']
     env.key_filename = '/Users/rossc/Projects/lawpal/lawpal-chef/chef-machines.pem'
 
+    env.restart_service = "kill -HUP `cat /tmp/lawpal.pid`"
 
 @task
 def stage():
     env.project = 'glynt'
     env.environment = 'stage'
-    env.project_path = '/home/stard0g101/webapps/%s' % env.project
+    env.local_project_path = os.path.dirname(os.path.realpath(__file__))
+    env.remote_project_path = '/home/stard0g101/webapps/%s' % env.project
+    env.deploy_archive_path = '~/'
 
     # change from the default user to 'vagrant'
     env.user = 'stard0g101'
@@ -54,9 +46,8 @@ def stage():
     env.key_filename = None
 
 
-@task
 def git_export():
-  cd(PROJECT_PATH)
+  cd(env.local_project_path)
   env.SHA1_FILENAME = local('git rev-parse --short --verify HEAD', capture=True)
   local('git archive --format zip --output /tmp/%s.zip --prefix=%s/ master' % (env.SHA1_FILENAME, env.SHA1_FILENAME,), capture=False)
 
@@ -65,47 +56,46 @@ def prepare_deploy():
     git_export()
 
 
+@task
+def chores():
+    sudo('aptitude install unzip')
+
 def deploy_archive_file():
-    put('/tmp/%s.zip'%(env.SHA1_FILENAME,), env.DEPLOY_ARCHIVE_PATH)
+    put('/tmp/%s.zip'%(env.SHA1_FILENAME,), env.deploy_archive_path)
 
 def conclude_deploy():
-    run('unlink %s%s.zip' % (env.DEPLOY_ARCHIVE_PATH, env.SHA1_FILENAME,))
+    run('unlink %s%s.zip' % (env.deploy_archive_path, env.SHA1_FILENAME,))
 
 
-@task
 def do_deploy():
-    environment = env.environment
-    app_name = env.app_name
-    project_name =env.project_name
-    remote_project_path = env.remote_project_path
-
     if env.SHA1_FILENAME is None:
         raise Exception('Must have a SHA1_FILENAME defined. Ensure you have run @git_export')
 
-    version_path = '%s/versions' % (remote_project_path,)
-    project_path = '%s/%s' % (remote_project_path, project_name,)
+    version_path = '%sversions' % env.remote_project_path
+    project_path = '%s%s' % (env.remote_project_path, env.project,)
 
     if not files.exists(version_path):
-        run('mkdir %s'%(version_path,))
+        sudo('mkdir -p %s' % version_path )
+    sudo('chown -R %s:%s %s' % (env.user,env.user, version_path) )
 
     deploy_archive_file()
 
     # extract project zip file:into a staging area and link it in
-    with cd('%s/' % (version_path,)):
-        run('unzip %s%s.zip' % (env.DEPLOY_ARCHIVE_PATH, env.SHA1_FILENAME,))
+    with cd('%s' % env.remote_project_path):
+        run('unzip %s%s.zip' % (env.deploy_archive_path, env.SHA1_FILENAME,))
 
-        if files.exists(project_path):
-            run('unlink %s'%(project_path,))
+        run('unlink %s'%(project_path,))
 
         run('ln -s %s/%s %s'%(version_path, env.SHA1_FILENAME, project_path,))
 
     # copy the live local_settings
     with cd(project_path):
-        run('cp %s/%s/conf/%s.local_settings.py %s/%s/%s/local_settings.py' % (remote_project_path, PROJECT, environment, remote_project_path, PROJECT, app_name,))
-        run('cp %s/%s/conf/%s.newrelic.ini %s/%s/newrelic.ini' % (remote_project_path, PROJECT, environment, remote_project_path, PROJECT,))
-        run('cp %s/%s/conf/%s.wsgi.py %s/%s/%s/wsgi.py' % (remote_project_path, PROJECT, environment, remote_project_path, PROJECT, app_name,))
+        print project_path
+        # run('cp conf/%s.local_settings.py %s/%s/%s/local_settings.py' % (environment, project_path, env.project, app_name,))
+        # run('cp conf/%s.newrelic.ini %s/newrelic.ini' % (environment, project_path,))
+        # run('cp conf/%s.wsgi.py %s/%s/wsgi.py' % (environment, project_path, env.project, app_name,))
 
-    run('%s/apache2/bin/restart' % (remote_project_path,))
+    run(env.restart_service)
 
 
 @task
@@ -116,7 +106,7 @@ def do_fixtures():
 
 @task
 def fixtures():
-    for app_name, project_name, remote_project_path in REMOTE_PROJECT_PATHS:
+    for app_name, project_name, remote_project_path in env.remote_project_path:
         env.app_name = app_name
         env.project_name = project_name
         env.remote_project_path = remote_project_path
@@ -131,7 +121,7 @@ def do_assets():
 
 @task
 def assets():
-    for app_name, project_name, remote_project_path in REMOTE_PROJECT_PATHS:
+    for app_name, project_name, remote_project_path in env.remote_project_path:
         env.app_name = app_name
         env.project_name = project_name
         env.remote_project_path = remote_project_path
@@ -150,7 +140,7 @@ def do_requirements():
 
 @task
 def requirements(deploy_to_env='staging'):
-    for app_name, project_name, remote_project_path in REMOTE_PROJECT_PATHS:
+    for app_name, project_name, remote_project_path in env.remote_project_path:
         env.app_name = app_name
         env.project_name = project_name
         env.remote_project_path = remote_project_path
@@ -159,11 +149,5 @@ def requirements(deploy_to_env='staging'):
 @task
 def deploy():
     prepare_deploy()
-
-    for app_name, project_name, remote_project_path in REMOTE_PROJECT_PATHS:
-        env.app_name = app_name
-        env.project_name = project_name
-        env.remote_project_path = remote_project_path
-
-        execute(do_deploy, hosts=env.hosts)
-        execute(conclude_deploy, hosts=env.hosts)
+    execute(do_deploy)
+    execute(conclude_deploy)
