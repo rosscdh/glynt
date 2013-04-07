7b3a3550da853e958c31b1bc1d403c7a87beafdb
Ross Crawford <sendrossemail@gmail.com>
2013-04-07 09:47:43 +0200

    fixign deprec error

--- conf/production.local_settings.py
+++ conf/production.local_settings.py
@@ -97,6 +97,11 @@ LOGGING = {
             'format': '%(levelname)s %(message)s'
         },
     },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'null': {
             'level': 'DEBUG',
@@ -114,6 +119,7 @@ LOGGING = {
         },
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
