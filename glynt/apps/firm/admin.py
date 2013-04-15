fcf1a2a0d565135246af97a353cef92bc01ba042
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-04-15 12:25:29 +0200

    made firm models less strict in their validation

--- glynt/apps/firm/admin.py
+++ glynt/apps/firm/admin.py
@@ -4,4 +4,12 @@ from django.contrib import admin
 from models import Firm, Office
 
 
-admin.site.register([Firm, Office])
+class FirmAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+#    list_filter = ['role']
+    list_display = ('name', 'slug', 'summary')
+    search_fields = ('name', 'slug', 'summary')
+    order = ('name')
+
+admin.site.register(Firm, FirmAdmin)
+admin.site.register([Office])
