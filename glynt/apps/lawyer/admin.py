f4c794b97b33a8eab7960c4a5b17047921b3d221
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-04-19 16:04:07 +0100

    fixed lawyer admin view

--- glynt/apps/lawyer/admin.py
+++ glynt/apps/lawyer/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 
 from models import Lawyer
 from glynt.apps.firm.models import Firm
-from django.contrib.auth.models import User
 
 
 class FirmInline(admin.TabularInline):
@@ -14,10 +13,11 @@ class FirmInline(admin.TabularInline):
 class LawyerAdmin(admin.ModelAdmin):
     list_filter = ['role']
     list_display = ('username', 'full_name', 'firm_name', 'email', 'summary', 'last_login')
-    search_fields = ('user__first_name', 'user__last_name', 'user__username', 'user__firm_lawyers__name')
+    search_fields = ('user__first_name', 'user__last_name', 'user__username', 'firm_lawyers__name')
     order = ('last_login')
     inlines = [
         FirmInline,
     ]
 
-admin.site.register(Lawyer,LawyerAdmin)
+
+admin.site.register(Lawyer, LawyerAdmin)
