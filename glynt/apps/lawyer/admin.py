963e9a149741a2461ebfd42b6ef26f2edcc15f20
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-04-19 17:44:17 +0100

    fixed lawyer serach

--- glynt/apps/lawyer/admin.py
+++ glynt/apps/lawyer/admin.py
@@ -13,7 +13,7 @@ class FirmInline(admin.TabularInline):
 class LawyerAdmin(admin.ModelAdmin):
     list_filter = ['role']
     list_display = ('username', 'full_name', 'primary_firm', 'email', 'summary', 'last_login')
-    search_fields = ('user__first_name', 'user__last_name', 'user__username', 'primary_firm')
+    search_fields = ('user__first_name', 'user__last_name', 'user__username', 'firm_lawyers__name')
     order = ('last_login')
     inlines = [
         FirmInline,
