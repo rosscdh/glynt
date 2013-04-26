7d8953c04b349032d0a5f9980a7671f0ca0f4efa
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-04-26 18:08:02 +0200

    Added the new flow. linked in -> confirm account -> welcome page

--- glynt/apps/client/forms.py
+++ glynt/apps/client/forms.py
@@ -3,7 +3,11 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm
 from django.template.defaultfilters import slugify
+from django.core import exceptions
 
+from django.contrib.auth.models import User
+
+from parsley.decorators import parsleyfy
 from bootstrap.forms import BootstrapMixin, Fieldset
 
 from userena.forms import SignupFormOnlyEmail
@@ -15,6 +19,28 @@ ACCEPTED_COUNTRIES = ('GB',)
 
 COUNTRIES_PLUS = [(i,c) for i,c in COUNTRIES_PLUS if i in ACCEPTED_COUNTRIES]
 
+
+@parsleyfy
+class ConfirmLoginDetailsForm(forms.ModelForm):
+    """ Shown to the user when they login using linked in 
+    Is the first form they see after signing in
+    """
+    username = forms.CharField(widget=forms.HiddenInput)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'confirm_password']
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+
+        if password != confirm_password:
+            raise exceptions.ValidationError(_('Passwords do not match'))
+        return password
 
 class SignupForm(BootstrapMixin, SignupFormOnlyEmail):
     """ The signup form overrides the Userena save method and hooks it up
