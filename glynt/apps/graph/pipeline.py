80c29876711bbba93b7e91ca00a791081a00fca2
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-04-30 10:00:08 +0200

    added better logging and fixed the infinite loop potential for social_auth logins

--- glynt/apps/graph/pipeline.py
+++ glynt/apps/graph/pipeline.py
@@ -23,16 +23,19 @@ def ensure_user_setup(*args, **kwargs):
 
 def graph_user_connections(backend, details, response, user=None, is_new=False,
                         *args, **kwargs):
-
+    logger.debug('Graph.graph_user_connections start')
     if user is not None:
         auth = UserSocialAuth.objects.get(user=user, provider=backend.name)
-        
+        logger.info('Graph.graph_user_connections auth: %s' % auth)
+
         try:
+            logger.info('Send: Graph.collect_user_fullcontact_info auth: %s' % user)
             collect_user_fullcontact_info.delay(user=user)
         except Exception as e:
             logger.error('Did not try collect_user_fullcontact_info as no connection to broker could be found: %s' % e)
 
         try:
+            logger.info('Send: Graph.collect_user_graph_connections auth: %s' % user)
             collect_user_graph_connections.delay(auth=auth)
         except Exception as e:
             logger.error('Did not try collect_user_graph_connections as no connection to broker could be found: %s' % e)
@@ -46,9 +49,13 @@ def get_username(details, user=None,
     if user was given.
     """
     if user:
+        logger.info('User exists: %s' % user.username)
         return {'username': user.username}
 
     uuid_length = getattr(settings, 'SOCIAL_AUTH_UUID_LENGTH', 4)
+    if uuid_length <= 0:
+        logger.error('uuid_length cannot be 0 or less: %d' % uuid_length)
+        uuid_length = 3
 
     if details.get('fullname'):
         username = unicode(details['fullname'])
@@ -58,6 +65,7 @@ def get_username(details, user=None,
         username = uuid.uuid4().get_hex()
 
     username = slugify(username)
+    logger.info('Getting username availability: %s' % username)
 
     max_length = UserSocialAuth.username_max_length()
     short_username = username[:max_length - uuid_length]
@@ -67,8 +75,11 @@ def get_username(details, user=None,
     # as base but adding a unique hash at the end. Original
     # username is cut to avoid any field max_length.
     while user_exists(username=final_username):
-        username = short_username + uuid.uuid4().get_hex()[:uuid_length]
+        logger.info('Username %s exists, trying to create another' % final_username)
+        username = '%s-%s' % (short_username, uuid.uuid4().get_hex()[:uuid_length])
         username = username[:max_length]
+
         final_username = slugify(UserSocialAuth.clean_username(username))
+
 
     return {'username': final_username}
