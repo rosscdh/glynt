2db78a752b109f1fff606092152887d5489add34
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-01 17:23:12 +0200

    renamed profile-thanks.html to invite.html cleaner more obvious

--- glynt/apps/lawyer/urls.py
+++ glynt/apps/lawyer/urls.py
@@ -9,7 +9,7 @@ from glynt.apps.lawyer.views import LawyerListView, LawyerProfileSetupView, Lawy
 urlpatterns = patterns('',
     # lawyers
     url(r'^profile/setup/$', login_required(LawyerProfileSetupView.as_view()), name='setup_profile'),
-    url(r'^profile/invite/$', login_required(TemplateView.as_view(template_name='lawyer/profile-thanks.html')), name='invite'),
+    url(r'^profile/invite/$', login_required(TemplateView.as_view(template_name='lawyer/invite.html')), name='invite'),
 
     url(r'^welcome/$', login_required(TemplateView.as_view(template_name='lawyer/welcome.html')), name='welcome'),
     url(r'^(?P<slug>.+)/$', LawyerProfileView.as_view(), name='profile'),
