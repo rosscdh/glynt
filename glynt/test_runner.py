a02faf60deb01d3a99dfb822a166e1e44b7c1aa5
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-04 23:13:57 +0200

    made testrunner a bit cleverer

--- glynt/test_runner.py
+++ glynt/test_runner.py
@@ -1,9 +1,10 @@
 # Make our own testrunner that by default only tests our own apps
 import os
+import sys
 from django.conf import settings
 
 # from django_behave.runner import DjangoBehaveTestSuiteRunner
-#from django_nose import NoseTestSuiteRunner
+# from django_nose import NoseTestSuiteRunner
 from django.test.simple import DjangoTestSuiteRunner
 
 
@@ -12,9 +13,13 @@ os.getenv('DJANGO_SETTINGS_MODULE', 'settings')
 
 class GlyntAppTestRunner(DjangoTestSuiteRunner):
     def build_suite(self, test_labels, *args, **kwargs):
-        PROJECT_APPS = []
-        # Remove path info and use only the app "label"
-        for app in settings.PROJECT_APPS:
-            app_name = app.split('.')[-1]
-            PROJECT_APPS.append(app_name)
+        PROJECT_APPS = sys.argv[2:]
+        
+        # not args passed in
+        if not PROJECT_APPS:
+            # Remove path info and use only the app "label"
+            for app in settings.PROJECT_APPS:
+                app_name = app.split('.')[-1]
+                PROJECT_APPS.append(app_name)
+
         return super(GlyntAppTestRunner, self).build_suite(test_labels or PROJECT_APPS, *args, **kwargs)
