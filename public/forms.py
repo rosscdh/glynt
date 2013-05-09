f5d73f4db57fd9a5d7e3159618a0d7c8d0a32fe2
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-09 14:16:16 +0200

    added init method to handle request and user and added authenticated check

--- public/forms.py
+++ public/forms.py
@@ -11,7 +11,13 @@ class ContactForm(BootstrapMixin, forms.Form):
     name = forms.CharField(help_text="", widget=forms.TextInput(attrs={'placeholder':'Name', 'tabindex':'1'}))
     message = forms.CharField(help_text="", widget=forms.Textarea(attrs={'placeholder':'Message', 'tabindex':'3'}))
 
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request')
+        self.user = self.request.user
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.inject_email()
+
     def inject_email(self):
-        """ If the user is NOT signed in inject email """
-        if not self.user.email:
+        """ If the user is NOT signed in OR they have no email address then show the email field """
+        if not self.user.is_authenticated() or not self.user.email:
             self.fields['email'] = forms.EmailField(help_text="", widget=forms.TextInput(attrs={'placeholder':'Email', 'tabindex':'2'}))
