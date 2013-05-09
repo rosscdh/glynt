e57a606275e8593338297b47ada60c3afed7edcb
Robert Balfre <emailcohort@gmail.com>
2013-05-09 13:04:14 +0100

    Attempting to inject email field if no user is present on the contact form

--- public/forms.py
+++ public/forms.py
@@ -9,10 +9,9 @@ from parsley.decorators import parsleyfy
 @parsleyfy
 class ContactForm(BootstrapMixin, forms.Form):
     name = forms.CharField(help_text="", widget=forms.TextInput(attrs={'placeholder':'Name', 'tabindex':'1'}))
-    email = forms.EmailField(help_text="", widget=forms.TextInput(attrs={'placeholder':'Email', 'tabindex':'2'}))
     message = forms.CharField(help_text="", widget=forms.Textarea(attrs={'placeholder':'Message', 'tabindex':'3'}))
 
-    def clean_email(self):
-        # TODO Need to check is user is logged in and if so fill in their email
-        data = self.cleaned_data['email']
-        return data
+    def inject_email(self):
+        """ If the user is NOT signed in inject email """
+        if not self.user.email:
+            self.fields['email'] = forms.EmailField(help_text="", widget=forms.TextInput(attrs={'placeholder':'Email', 'tabindex':'2'}))
