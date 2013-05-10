13b92fdd8dd1e550f1980b78a4769838f145d309
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-10 16:07:42 +0200

    Upgraded contact-us tests

--- public/tests.py
+++ public/tests.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
 from django.test import LiveServerTestCase
 from django.test.client import Client
 from django.test.utils import override_settings
-from django import template
+from django.core.urlresolvers import reverse
+
+from glynt.tests import TemplateRendererMixin
 
 from BeautifulSoup import BeautifulSoup
 
+from django.contrib.auth.models import User
 from glynt.apps.factories import UserFactory, LoggedOutUserFactory
 
 
-class TestContactUsForm(LiveServerTestCase):
+class TestContactUsForm(LiveServerTestCase, TemplateRendererMixin):
     fixtures = ['test_cities']
+
     def setUp(self):
         self.client = Client()
+        self.contact_us_url = reverse('public:contact_us')
+        self.user = User.objects.create_user('test', 'test@lawpal.com', 'test')
 
-        self.user = UserFactory.create()
         self.context = {
             'user': self.user
         }
@@ -23,15 +29,40 @@ class TestContactUsForm(LiveServerTestCase):
             'user': self.loggedout_user
         }
 
-    def render_template(self, *args, **kwargs):
-        context = kwargs.get('context', {})
-        t = template.Template(''.join(args))
-        c = template.Context(context)
-        return t.render(c)
-
-    @override_settings(PROJECT_ENVIRONMENT='test')
     def test_presence_loggedin(self):
-        result = self.client.get('/contact-us/')
+        """ when not logged in the user should not see the email input """
+        logged_in = self.client.login(username=self.user.username, password='test')
+        self.assertTrue(logged_in)
+        self.assertIn('_auth_user_id', self.client.session)
+
+        result = self.client.get(self.contact_us_url)
+
         soup = BeautifulSoup(result.content)
-        emailForm = soup.findAll('input', id="id_email")
-        self.assertTrue(1 == 1)
+        hiddenEmailElement = soup.findAll('input', id="id_email", type="hidden") # hidden input
+
+        self.assertTrue(len(hiddenEmailElement) == 1)
+
+    def test_presence_loggedin_but_no_email(self):
+        """ when logged in but if the user has no email address then they should see the email input """
+        user = User.objects.create_user('test-no-email', '', 'test') # user with no email
+        logged_in = self.client.login(username=user.username, password='test')
+        self.assertTrue(logged_in)
+        self.assertIn('_auth_user_id', self.client.session)
+
+        result = self.client.get(self.contact_us_url)
+
+        soup = BeautifulSoup(result.content)
+        emailElement = soup.findAll('input', id="id_email", type="text") # normal input
+
+        self.assertTrue(len(emailElement) == 1)
+
+    def test_presence_not_loggedin(self):
+        """ when not logged in the user should see the email input """
+        self.assertNotIn('_auth_user_id', self.client.session) # not logged in
+
+        result = self.client.get(self.contact_us_url)
+
+        soup = BeautifulSoup(result.content)
+        emailElement = soup.findAll('input', id="id_email", type="text") # normal input
+
+        self.assertTrue(len(emailElement) == 1)
