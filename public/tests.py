e5b3869c0c299a22663143ea8cb5a5d2b158f9ef
Robert Balfre <emailcohort@gmail.com>
2013-05-10 10:43:26 +0100

    tests.py init
    Not sure how to test for the email form

--- public/tests.py
+++ public/tests.py
@@ -1,16 +1,37 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.test import LiveServerTestCase
+from django.test.client import Client
+from django.test.utils import override_settings
+from django import template
 
-Replace this with more appropriate tests for your application.
-"""
+from BeautifulSoup import BeautifulSoup
 
-from django.test import TestCase
+from glynt.apps.factories import UserFactory, LoggedOutUserFactory
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestContactUsForm(LiveServerTestCase):
+    fixtures = ['test_cities']
+    def setUp(self):
+        self.client = Client()
+
+        self.user = UserFactory.create()
+        self.context = {
+            'user': self.user
+        }
+
+        self.loggedout_user = LoggedOutUserFactory.create()
+        self.loggedout_context = {
+            'user': self.loggedout_user
+        }
+
+    def render_template(self, *args, **kwargs):
+        context = kwargs.get('context', {})
+        t = template.Template(''.join(args))
+        c = template.Context(context)
+        return t.render(c)
+
+    @override_settings(PROJECT_ENVIRONMENT='test')
+    def test_presence_loggedin(self):
+        result = self.client.get('/contact-us/')
+        soup = BeautifulSoup(result.content)
+        emailForm = soup.findAll('input', id="id_email")
+        self.assertTrue(1 == 1)
