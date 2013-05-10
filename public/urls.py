e9a32a08bf1557cec7294e12c687189671cfb6d0
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-10 09:18:41 +0200

    removed redundant FormView

--- public/urls.py
+++ public/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView
 
 from views import LoggedInRedirectView, PublicHomepageView, ContactUsView, ThankYouView
 
