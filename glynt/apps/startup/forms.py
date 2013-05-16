061fd249b2d6910a8730889feae0c0c5d5df9250
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-16 14:48:00 +0200

    getting form into slightly better shape

--- glynt/apps/startup/forms.py
+++ glynt/apps/startup/forms.py
@@ -5,19 +5,39 @@ from bootstrap.forms import BootstrapMixin
 from parsley.decorators import parsleyfy
 from models import Startup, Founder
 
+from services import EnsureStartupService
+
 import logging
 logger = logging.getLogger('django.request')
 
 
 @parsleyfy
 class StartupProfileSetupForm(BootstrapMixin, forms.Form):
+    """ Form to allow startups to enter basic information about 
+    their setups
+    """
     first_name = forms.CharField(help_text="", widget=forms.TextInput(attrs={'placeholder':'First name', 'tabindex':'1'}))
     last_name = forms.CharField(help_text="", widget=forms.TextInput(attrs={'placeholder':'Last name','tabindex':'2'}))
-    email = forms.EmailField(help_text="", widget=forms.TextInput(attrs={'data-trigger':'change','placeholder':'Email address', 'tabindex':'3'}))
-    password = forms.CharField(label="Password", help_text="", widget=forms.PasswordInput(attrs={'placeholder':'Enter your password', 'data-trigger':'change', 'tabindex':'4'}))
-    password_confirm = forms.CharField(label="", help_text="", widget=forms.PasswordInput(attrs={'placeholder':'Enter your password again', 'data-trigger':'change', 'minLength':'5', 'data-equalto':'#id_password', 'tabindex':'5'}))
-    name = forms.CharField(label="Acme Inc", help_text="", widget=forms.TextInput(attrs={'placeholder':'Full Name', 'tabindex':'6'}))
+    startup_name = forms.CharField(label="Acme Inc", help_text="", widget=forms.TextInput(attrs={'placeholder':'Startup Name', 'tabindex':'6'}))
     already_raised_capital = forms.BooleanField(required=False, label='', widget=forms.CheckboxInput(attrs={'tabindex':'7'}))
     process_raising_capital = forms.BooleanField(required=False, label='', widget=forms.CheckboxInput(attrs={'tabindex':'8'}))
-    incubator_or_accelerator = forms.BooleanField(required=False, label='', widget=forms.CheckboxInput(attrs={'class':'incubator', 'tabindex':'9'}))
+    incubator_or_accelerator = forms.CharField(required=False, label='', widget=forms.TextInput(attrs={'class':'incubator', 'tabindex':'9'}))
     agree_tandc = forms.BooleanField(label='', widget=forms.CheckboxInput(attrs={'tabindex':'10'}))
+
+    def __init__(self, *args, **kwargs):
+        """ get request object and user """
+        self.request = kwargs.pop('request', None)
+        self.user = self.request.user
+        super(StartupProfileSetupForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        data = self.cleaned_data
+
+        #self.user
+        logger.info('StartupProfileSetupForm Starting')
+
+        founder_service = EnsureFounderService(founder_user=self.user)
+        founder = founder_service.process()
+
+        startup_service = EnsureStartupService(founder=founder, **data)
+        startup = startup_service.process()
