97f9c0145f30d55cd7a28cb78ea40185b94898c2
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-17 12:12:07 +0200

    got the view instantiating the form and populating its data not the new fields whoever

--- glynt/apps/startup/views.py
+++ glynt/apps/startup/views.py
@@ -3,7 +3,7 @@ from django.views.generic import FormView
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 
-from services import EnsureFounderService, EnsureStartupService
+from services import EnsureFounderService
 from forms import StartupProfileSetupForm
 
 import urlparse
@@ -27,15 +27,26 @@ class StartupProfileSetupView(FormView):
         kwargs.update({'request': self.request}) # add the request to the form
         user = self.request.user
 
-        founder_service = EnsureFounderService(user=user)
+        founder_service = EnsureFounderService(founder_user=user)
         founder = founder_service.process()
-        # if founder:
-        #     startup_service = EnsureStartupService(founder=founder)
-        #     startup = startup_service.process()
+        # get the startup
+        startup = founder.primary_startup
 
         kwargs.update({'initial': {
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'agree_tandc': None,
+            'first_name': founder.user.first_name,
+            'last_name': founder.user.last_name,
+            'startup_name': startup.name,
+            
+            'incubator_or_accelerator_name': founder.data.get('agree_tandc'),
+            'already_raised_capital': founder.data.get('already_raised_capital'),
+            'process_raising_capital': founder.data.get('process_raising_capital'),
+
+            'incubator_or_accelerator_name': founder.data.get('incubator_or_accelerator_name'),
+
+            'agree_tandc': founder.data.get('agree_tandc', False),
         }})
         return form_class(**kwargs)
+
+    def form_valid(self, form):
+        form.save()
+        return super(StartupProfileSetupView, self).form_valid(form=form)
