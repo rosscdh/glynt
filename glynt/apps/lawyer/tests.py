482e3609c2ff4c59519cc97ccec615559c156c9f
Robert Balfre <emailcohort@gmail.com>
2013-05-21 11:08:04 +0100

    small JS fix for profile form and added test for humaniseNumber template tag

--- glynt/apps/lawyer/tests.py
+++ glynt/apps/lawyer/tests.py
@@ -6,6 +6,10 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django import template
+from django.test.utils import override_settings
+
+import unittest
 
 
 class SimpleTest(TestCase):
@@ -14,3 +18,27 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class TestHumaniseNumber(unittest.TestCase):
+    def setUp(self):
+        self.obj = '500k'
+        self.context = {
+            'big_number': 500000
+        }
+
+    def render_template(self, *args, **kwargs):
+        context = kwargs.get('context', {})
+        t = template.Template(''.join(args))
+        c = template.Context(context)
+        return t.render(c)
+
+    @override_settings(PROJECT_ENVIRONMENT='test')
+    def test_humanise(self):
+        result = self.render_template(
+            '{% load lawyer_profile_tags %}'
+            ,'{% humanise_number num=big_number %}'
+            , context=self.context
+        )
+
+        self.assertEqual(result, self.obj)
