72fb5f357949780cd7cdf795e62ea470cfa7ac0d
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-23 18:29:39 +0200

    getting form setup correctly

--- glynt/apps/engage/forms.py
+++ glynt/apps/engage/forms.py
@@ -1,6 +1,20 @@
-from postman.forms import WriteForm
+# -*- coding: utf-8 -*-
 from django import forms
+from postman.forms import WriteForm
+from postman.fields import CommaSeparatedUserField
 
 
 class EngageWriteMessageForm(WriteForm):
-    pass
+    recipients = CommaSeparatedUserField(label='Recipient', required=False, widget=forms.HiddenInput)
+
+    def __init__(self, *args, **kwargs):
+        """ get request object and user """
+        self.request = kwargs.pop('request', None)
+        self.user = self.request.user
+        self.to = kwargs.pop('to', None)
+        kwargs['sender'] = kwargs.pop('from', None)
+
+        recipients = [self.to.username]
+
+        super(EngageWriteMessageForm, self).__init__(*args, **kwargs)
+        self.fields['recipients'].initial = recipients
