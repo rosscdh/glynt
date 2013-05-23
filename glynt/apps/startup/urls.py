78920af7489ecff3f93336f1a5e41820e2015eae
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-23 08:58:24 +0200

    redirect startups to lawyer list as default behaviour after saving profile

--- glynt/apps/startup/urls.py
+++ glynt/apps/startup/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 from views import StartupProfileSetupView
 
@@ -9,7 +11,8 @@ from views import StartupProfileSetupView
 urlpatterns = patterns('',
     url(r'^profile/setup/$', login_required(StartupProfileSetupView.as_view()), name='setup_profile'),
     url(r'^invite/$', login_required(TemplateView.as_view(template_name='startup/invite.html')), name='invite'),
-    url(r'^$', login_required(TemplateView.as_view(template_name='startup/welcome.html')), name='welcome'),
+    #url(r'^$', login_required(TemplateView.as_view(template_name='startup/welcome.html')), name='welcome'),
+    url(r'^$', login_required(RedirectView.as_view(url=reverse_lazy('lawyer:list'))), name='welcome'),
     # no profile for startups #url(r'^(?P<slug>.+)/$', login_required(LawyerProfileView.as_view()), name='profile'),
     # also no list for startups #url(r'^$', login_required(LawyerListView.as_view()), name='list'),
 )
