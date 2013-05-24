fc781af7dfb330ef88864741a0d7bcd5cf8c6855
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-05-24 17:19:58 +0200

    added authentication check to prevent redirect loop

--- public/views.py
+++ public/views.py
@@ -40,7 +40,7 @@ class PublicHomepageView(TemplateView):
         # @BUSINESS_RULE
         # redirect the startup to the marketplace/lawyer-list
         user_class_name = self.request.session.get('user_class_name', 'lawyer')
-        if user_class_name == 'startup':
+        if self.request.user.is_authenticated() and user_class_name == 'startup':
             messages.success(self.request, "Welcome back to LawPal.com")
             return HttpResponseRedirect(redirect_to=reverse('lawyer:list'))
         else:
