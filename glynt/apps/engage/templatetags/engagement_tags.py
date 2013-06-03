49db5075790f0a3106d370bacc9b0429fbd07599
Robert Balfre <emailcohort@gmail.com>
2013-06-03 15:02:45 +0100

    Refactored engage tags to reuse similar code

--- glynt/apps/engage/templatetags/engagement_tags.py
+++ glynt/apps/engage/templatetags/engagement_tags.py
@@ -15,19 +15,30 @@ def engagement_intro(context, enagement):
     return context
 
 
+def get_engagement_list(data, lawyer):
+    engagement_list = []
+    own_profile = True if lawyer.user.pk == data.get('user').pk else False
+
+    if data.get('user',None) is not None:
+        if not own_profile:
+            if data.get('user').profile.is_startup:
+                engagement_list = Engagement.objects.filter(lawyer=lawyer, founder=data.get('user').founder_profile)
+
+    data = {
+        'engagement_list': engagement_list,
+        'own_profile' : own_profile,
+    }
+
+    return data
+
+
 @register.inclusion_tag('engage/partials/engagement_with_lawyer.html', takes_context=True)
 def engagement_with_lawyer(context, lawyer):
-    engagement_list = []
-    own_profile = True if lawyer.user.pk == context.get('user').pk else False
-
-    if context.get('user',None) is not None:
-        if not own_profile:
-            if context.get('user').profile.is_startup:
-                engagement_list = Engagement.objects.filter(lawyer=lawyer, founder=context.get('user').founder_profile)
+    data = get_engagement_list(context, lawyer)
 
     context.update({
-        'engagement_list': engagement_list,
-        'own_profile': own_profile,
+        'engagement_list': data.get('engagement_list'),
+        'own_profile': data.get('own_profile'),
         'lawyer': lawyer,
     })
     return context
@@ -36,12 +47,9 @@ def engagement_with_lawyer(context, lawyer):
 @register.assignment_tag(takes_context=True)
 def is_lawyer_engaged(context, lawyer):
     lawyer_enganged = False
+    data = get_engagement_list(context, lawyer)
 
-    own_profile = True if lawyer.user.pk == context.get('user').pk else False
-
-    if context.get('user',None) is not None:
-        if not own_profile:
-            if context.get('user').profile.is_startup:
-                lawyer_enganged = True if Engagement.objects.filter(lawyer=lawyer, founder=context.get('user').founder_profile) else False
+    if data.get('engagement_list'):
+        lawyer_enganged = True
 
     return lawyer_enganged
