6261bee5c0459900080e6b792d9c43ccba1fc0f2
Robert Balfre <emailcohort@gmail.com>
2013-06-03 14:31:25 +0100

    added assignment_tag to check if lawyer has been engaged

--- glynt/apps/engage/templatetags/engagement_tags.py
+++ glynt/apps/engage/templatetags/engagement_tags.py
@@ -31,3 +31,17 @@ def engagement_with_lawyer(context, lawyer):
         'lawyer': lawyer,
     })
     return context
+
+
+@register.assignment_tag(takes_context=True)
+def is_lawyer_engaged(context, lawyer):
+    lawyer_enganged = False
+
+    own_profile = True if lawyer.user.pk == context.get('user').pk else False
+
+    if context.get('user',None) is not None:
+        if not own_profile:
+            if context.get('user').profile.is_startup:
+                lawyer_enganged = True if Engagement.objects.filter(lawyer=lawyer, founder=context.get('user').founder_profile) else False
+
+    return lawyer_enganged
