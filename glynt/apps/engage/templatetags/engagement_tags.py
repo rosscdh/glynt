746a923b1ca49cac90e6630de4566373a7acec6f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-06-03 09:01:48 +0200

    Added engagement tempalte_tag

--- glynt/apps/engage/templatetags/engagement_tags.py
+++ glynt/apps/engage/templatetags/engagement_tags.py
@@ -4,10 +4,30 @@ from django.core.urlresolvers import reverse
 
 register = template.Library()
 
+from glynt.apps.engage.models import Engagement
+
 
 @register.inclusion_tag('engage/partials/engagement_intro.html', takes_context=True)
 def engagement_intro(context, enagement):
     context.update({
         'enagement': enagement,
+    })
+    return context
+
+
+@register.inclusion_tag('engage/partials/engagement_with_lawyer.html', takes_context=True)
+def engagement_with_lawyer(context, lawyer):
+    engagement_list = []
+    own_profile = True if lawyer.user.pk == context.get('user').pk else False
+
+    if context.get('user',None) is not None:
+        if not own_profile:
+            if context.get('user').profile.is_startup:
+                engagement_list = Engagement.objects.filter(lawyer=lawyer, founder=context.get('user').founder_profile)
+
+    context.update({
+        'engagement_list': engagement_list,
+        'own_profile': own_profile,
+        'lawyer': lawyer,
     })
     return context
