81a7fa77621c386838990aab93eba28aed7bd173
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-06-10 17:44:18 +0200

    converted to use slug and not pk for engagement

--- glynt/apps/engage/urls.py
+++ glynt/apps/engage/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
     # url(r'^(?P<to>.+)/message/$', EngageWriteMessageView.as_view(), name='message'),
     url(r'^lawyer/(?P<lawyer_pk>\d+)/as/startup/$', login_required(StartupEngageLawyerView.as_view()), name='startup_lawyer'),
     
-    url(r'^(?P<pk>\d+)/close/$', login_required(CloseEngagementView.as_view()), name='close'),
-    url(r'^(?P<pk>\d+)/re-open/$', login_required(ReOpenEngagementView.as_view()), name='re-open'),
+    url(r'^(?P<slug>.+)/close/$', login_required(CloseEngagementView.as_view()), name='close'),
+    url(r'^(?P<slug>.+)/re-open/$', login_required(ReOpenEngagementView.as_view()), name='re-open'),
     url(r'^(?P<slug>.+)/$', login_required(EngagementView.as_view()), name='engagement'),
     url(r'^$', login_required(MyEngagementsView.as_view()), name='list'),
 )
