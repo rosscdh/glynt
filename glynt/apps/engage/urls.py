ebca4abe49eb0cbe830f0e1b0481568e85d34d53
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-06-11 06:53:38 +0200

    cleaed unused imports

--- glynt/apps/engage/urls.py
+++ glynt/apps/engage/urls.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
 
 from views import StartupEngageLawyerView, EngagementView, CloseEngagementView, ReOpenEngagementView, MyEngagementsView
 
