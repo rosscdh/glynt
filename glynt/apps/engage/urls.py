df83d6032c5036b58f4c065bc12aa2b7dfe01e7d
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-06-11 06:52:34 +0200

    Cleaned urls

--- glynt/apps/engage/urls.py
+++ glynt/apps/engage/urls.py
@@ -6,10 +6,7 @@ from django.views.generic import TemplateView
 from views import StartupEngageLawyerView, EngagementView, CloseEngagementView, ReOpenEngagementView, MyEngagementsView
 
 urlpatterns = patterns('',
-    # enage message
-    # url(r'^(?P<to>.+)/message/$', EngageWriteMessageView.as_view(), name='message'),
     url(r'^lawyer/(?P<lawyer_pk>\d+)/as/startup/$', login_required(StartupEngageLawyerView.as_view()), name='startup_lawyer'),
-    
     url(r'^(?P<slug>.+)/close/$', login_required(CloseEngagementView.as_view()), name='close'),
     url(r'^(?P<slug>.+)/re-open/$', login_required(ReOpenEngagementView.as_view()), name='re-open'),
     url(r'^(?P<slug>.+)/$', login_required(EngagementView.as_view()), name='engagement'),
