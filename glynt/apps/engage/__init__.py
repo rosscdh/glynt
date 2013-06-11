4686c823af02770779bf3b78ca5799c13ae6ae46
Robert Balfre <emailcohort@gmail.com>
2013-06-11 11:15:39 +0100

    removed duplicate code in __init__

--- glynt/apps/engage/__init__.py
+++ glynt/apps/engage/__init__.py
@@ -3,16 +3,6 @@ import hashlib
 import datetime
 from glynt.apps.utils import get_namedtuple_choices
 
-
-ENGAGEMENT_STATUS = get_namedtuple_choices('ENGAGEMENT_STATUS', (
-    (0, 'new', 'New'),
-    (1, 'open', 'Open'),
-    (2, 'closed', 'Closed'),
-))
-
-from glynt.apps.utils import get_namedtuple_choices
-
-
 ENGAGEMENT_STATUS = get_namedtuple_choices('ENGAGEMENT_STATUS', (
     (0, 'new', 'New'),
     (1, 'open', 'Open'),
