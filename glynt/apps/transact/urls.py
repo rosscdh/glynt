ec9b9cb8bab20502769c40ede7f9f35a26d084aa
Robert Balfre <emailcohort@gmail.com>
2013-06-18 21:13:02 +0100

    added placeholder template for second_stage and renamed the first step

--- glynt/apps/transact/urls.py
+++ glynt/apps/transact/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='transaction/transactions.html'), name='design'),
+    url(r'^$', TemplateView.as_view(template_name='transaction/selection.html'), name='selection'),
+    url(r'^second-stage/$', TemplateView.as_view(template_name='transaction/second_stage_tmp.html'), name='second_stage_tmp'),
 )
