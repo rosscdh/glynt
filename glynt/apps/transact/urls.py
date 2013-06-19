a89abf68e8e9d3c31180a3184eb8baf4d14d97f4
Robert Balfre <emailcohort@gmail.com>
2013-06-19 15:22:24 +0100

    url for example template and removed second-stage

--- glynt/apps/transact/urls.py
+++ glynt/apps/transact/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='transaction/selection.html'), name='selection'),
-    url(r'^second-stage/$', TemplateView.as_view(template_name='transaction/second_stage_tmp.html'), name='second_stage_tmp'),
+    url(r'^intake/$', TemplateView.as_view(template_name='transaction/intake_questionnaire_tmp.html'), name='intake'),
 )
