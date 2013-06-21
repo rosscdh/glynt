d18c5696d8d5a7d1c9139e66d3eec88a04a4a5b3
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-06-21 12:30:54 +0200

    updated dash urls to make use of todo app views

--- glynt/apps/dashboard/urls.py
+++ glynt/apps/dashboard/urls.py
@@ -7,8 +7,8 @@ from glynt.apps.todo.views import MyToDoListView, ToDoDetailView
 
 
 urlpatterns = patterns('',
-    url(r'^checklist/(?P<id>[a-zA-Z0-9]{24})$', login_required(ToDoDetailView.as_view()), name='item'),
-    url(r'^checklist$', login_required(MyToDoListView.as_view()), name='checklist'),
-    url(r'^documents$', login_required(TemplateView.as_view(template_name='dashboard/documents.html')), name='documents'),
+    url(r'^checklist/(?P<slug>.+)/$', login_required(ToDoDetailView.as_view()), name='item'),
+    url(r'^checklist/$', login_required(MyToDoListView.as_view()), name='checklist'),
+    url(r'^documents/$', login_required(TemplateView.as_view(template_name='dashboard/documents.html')), name='documents'),
     url(r'^$', login_required(TemplateView.as_view(template_name='dashboard/overview.html')), name='overview'),
 )
