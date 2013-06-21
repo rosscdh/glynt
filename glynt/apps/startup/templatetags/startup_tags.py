c163f857f46ddeeaef717d562582c95c7abdee7b
Robert Balfre <emailcohort@gmail.com>
2013-06-21 10:19:50 +0100

    tidying up template tag

--- glynt/apps/startup/templatetags/startup_tags.py
+++ glynt/apps/startup/templatetags/startup_tags.py
@@ -5,9 +5,7 @@ register = template.Library()
 
 @register.filter
 def as_percentage_of(part, whole):
-    #whole = int(float(whole))
     try:
         return "%d%%" % (int(float(part)) / int(float(whole)) * 100)
     except (ValueError, ZeroDivisionError):
-        print 'yup'
         return ""
