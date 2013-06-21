00a6e4a611913c0edc10d66daefbf12dc8c1dd00
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-06-21 10:21:15 +0200

    added todo app view and urls to be standalone

--- glynt/apps/todo/views.py
+++ glynt/apps/todo/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.views.generic import ListView, DetailView
+
+from glynt.apps.todo.models import ToDo
+
+
+class MyToDoListView(ListView):
+    model = ToDo
+    paginate_by = 10
+
+    def get_queryset(self):
+        """
+        Provide a list of todos for the current user
+        """
+        queryset = self.model.objects.filter(user=self.request.user).all()
+        return queryset
+
+
+class ToDoDetailView(ListView):
+    model = ToDo
