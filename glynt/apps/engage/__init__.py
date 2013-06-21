68743b6a6c809ca73f022c7d5fb45ad93066540a
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-06-21 13:45:35 +0200

    updated transact fixtures and created migration for Engagement FK to Transaction

--- glynt/apps/engage/__init__.py
+++ glynt/apps/engage/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from glynt.apps.utils import get_namedtuple_choices
 
+
 ENGAGEMENT_STATUS = get_namedtuple_choices('ENGAGEMENT_STATUS', (
     (0, 'new', 'New'),
     (1, 'open', 'Open'),
