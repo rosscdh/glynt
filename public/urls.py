5f3c543ef98e2b524d813c981ac4e615bb0152c5
Alex Halliday <amhalliday@gmail.com>
2013-06-28 18:26:25 -0700

    Updated bootstrap3, bringing it up to date.

--- public/urls.py
+++ public/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 from views import PublicHomepageView, ContactUsView, \
                     UserClassSessionRedirectView, UserClassLoggedInRedirectView
