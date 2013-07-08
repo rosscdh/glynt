2f3eb7ed436fe7c55d5b2ea810ee679d3fd88251
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-08 15:40:27 +0200

    Added cleaner path vars in init

---
+++ glynt/apps/transact/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+import os
+
+MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+FLYFORM_TEMPLATE_PATH = os.path.join(MODULE_PATH, 'templates/flyforms/')
+TRANSACTION_TEMPLATE_PATH = os.path.join(MODULE_PATH, 'templates/transactions/')
