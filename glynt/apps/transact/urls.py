e0405ce037e9a993daf7ade0e7f3cdcdac13d4f9
Alex Halliday <amhalliday@gmail.com>
2013-07-09 19:41:23 -0700

    Further conversion to Bootstrap3…. getting there!

--- glynt/apps/transact/urls.py
+++ glynt/apps/transact/urls.py
@@ -2,8 +2,8 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from views import PackagesWizard, FORMS
+# from views import PackagesWizard, FORMS
 
 urlpatterns = patterns('',
-    url(r'^create/$', login_required(PackagesWizard.as_view(FORMS)), name='packages'),
+    # url(r'^create/$', login_required(PackagesWizard.as_view(FORMS)), name='packages'),
 )
