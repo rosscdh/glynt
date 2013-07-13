3f2562d46a4be314d5a28bbbfcc2e1abe00b22b3
Jamie Connolly <jamie@lawpal.com>
2013-07-13 09:26:56 +0100

    Add in the open item view

--- glynt/apps/dashboard/urls.py
+++ glynt/apps/dashboard/urls.py
@@ -9,7 +9,10 @@ from glynt.apps.dashboard.views import CustomerDashboardView
 
 urlpatterns = patterns('',
     url(r'^matching/$', login_required(TemplateView.as_view(template_name="dashboard/matching.html")), name='matching'),
-    url(r'^checklist/(?P<slug>.+)/$', login_required(ToDoDetailView.as_view()), name='item'),
+    url(r'^checklist/open/$', login_required(TemplateView.as_view(template_name='todo/item/open.html')), name='item-open'),
+    url(r'^checklist/pending/$', login_required(TemplateView.as_view(template_name='todo/item/pending.html')), name='item-pending'),
+    url(r'^checklist/resolved/$', login_required(TemplateView.as_view(template_name='todo/item/resolved.html')), name='item-resolved'),
+    # url(r'^checklist/(?P<slug>.+)/$', login_required(ToDoDetailView.as_view()), name='item'),
     url(r'^checklist/$', login_required(MyToDoListView.as_view()), name='checklist'),
     url(r'^documents/$', login_required(TemplateView.as_view(template_name='dashboard/documents.html')), name='documents'),
     url(r'^$', login_required(CustomerDashboardView.as_view()), name='overview'),
