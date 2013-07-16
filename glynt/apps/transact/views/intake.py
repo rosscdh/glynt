d0a2aabd302ffe29c19fe74a80310e3e01632e3a
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-16 14:45:40 +0200

    Created company profile form and setup the transact process to use it as the first INTAKE form

--- glynt/apps/transact/views/intake.py
+++ glynt/apps/transact/views/intake.py
@@ -1,18 +1,22 @@
 # -*- coding: UTF-8 -*-
-from glynt.apps.transact.forms import BasicInformationForm, CorporateAgentsForm, \
-    InitialDirectorsForm, GeneralCapitalizationForm, CustomersForm, StockPlansForm, AboutCompanyBusinessForm, \
-    IntellectualPropertyForm, EmployeesConsultantsForm
+from glynt.apps.transact.forms import CompanyProfileForm
+
+# from glynt.apps.transact.forms import BasicInformationForm, CorporateAgentsForm, \
+#     InitialDirectorsForm, GeneralCapitalizationForm, CustomersForm, StockPlansForm, AboutCompanyBusinessForm, \
+#     IntellectualPropertyForm, EmployeesConsultantsForm
 
 
-FORMS = [("basic_information", BasicInformationForm),
-        ("corporate_agents", CorporateAgentsForm),
-        ("initial_directors",InitialDirectorsForm),
-        ("general_capitalization", GeneralCapitalizationForm),
-        ("customers",CustomersForm),
-        ("stock_plans", StockPlansForm),
-        ("about_company_business", AboutCompanyBusinessForm),
-        ("intellectual_property", IntellectualPropertyForm),
-        ("employees_consultants", EmployeesConsultantsForm)]
+FORMS = [("company_profile", CompanyProfileForm), ]
+
+# FORMS = [("basic_information", BasicInformationForm),
+#         ("corporate_agents", CorporateAgentsForm),
+#         ("initial_directors",InitialDirectorsForm),
+#         ("general_capitalization", GeneralCapitalizationForm),
+#         ("customers",CustomersForm),
+#         ("stock_plans", StockPlansForm),
+#         ("about_company_business", AboutCompanyBusinessForm),
+#         ("intellectual_property", IntellectualPropertyForm),
+#         ("employees_consultants", EmployeesConsultantsForm)]
 
 
 TEMPLATES = []
