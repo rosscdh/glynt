c5def568d55be7cbd863a303c01340c0090ee0e4
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-19 18:34:53 +0200

    working add and remove (basic) of items from the JSON via controls. Advanced use of events

---
+++ glynt/apps/transact/static/transact/js/jquery.plugin.form_json_data.js
@@ -0,0 +1,123 @@
+'use strict';
+/**
+* Manage the JSON data field of a form
+* ------------------------------------
+* 
+*/
+$(function() {
+    // the widget definition, where "custom" is the namespace,
+    $.widget( "lawpal.form_json_data", {
+        // default options
+        options: {
+            debug: true,
+            update_url: undefined,
+            source_data: {} // data provided by the form being viewed
+        },
+        form_json_data: {},
+        form: null,
+        _log: function (msg) {
+            if (self.options.debug === true) {
+                console.log(msg)
+            }
+        },
+        // the constructor
+        _create: function () {
+            var self = this;
+            this.form = $(this.element).closest('form');
+
+            this.options.source_data = JSON.parse(this.element.val()) || this.options.source_data;
+
+            $(document).on("ready", function (event) {
+
+                /**
+                * Send off the reference to teh source data.
+                * may be modified by various listeners
+                */
+                $.event.trigger({
+                    type: "LOADED_form_json_data",
+                    form_json_data: self.options.source_data
+                });
+
+                self._listen();
+
+                self._refresh()
+            });
+        },
+        _listen: function () {
+            var self = this;
+            /**
+            * Capture all changes made to the source_data
+            */
+            if (self.options.update_url) {
+                /**
+                * Capture changes made to the form_json_data
+                */
+                $(document).on("SAVE_form_json_data", function (event) {
+console.log(self.options.update_url)
+                    $.ajax({
+                        type: 'POST',
+                        url: self.options.update_url,
+                        data: self.options.source_data,
+                    })
+                    .success(function(data, textStatus, jqXHR) {
+                        self._log('success SAVE_form_json_data')
+                    })
+                    .error(function(jqXHR, textStatus, errorThrown) { 
+                        self._log('error SAVE_form_json_data')
+                    })
+                    .complete(function() {
+                        self._log('complete SAVE_form_json_data')
+                    });
+                });
+            } else {
+                self._log('self.options.update_url does not exist');
+            }
+
+            $(document).on("REMOVE_CLONED_REGION_form_json_data", function (event) {
+                var cloned_region_key = event.cloned_region_key;
+
+                $.each($(event.cloned_region).find('input, select, checkbox, radio'), function (i, elem) {
+                    elem = $(elem);
+                    var elem_id = elem.prop('id');
+
+                    delete self.options.source_data[cloned_region_key][elem_id];
+                });
+
+                self._refresh()
+            });
+            /**
+            * Capture changes made to Cloned Region items
+            */
+            $(document).on("MODIFIED_CLONED_REGION_form_json_data", function (event) {
+                var elem = $(event.instance);
+                var cloned_region_key = event.cloned_region_key;
+                var elem_id = elem.prop('id');
+                var instance_dict = {'id': elem.prop('id'), 'name': elem.prop('name'), 'val': elem.val()}
+
+                self.options.source_data[cloned_region_key][elem_id] = instance_dict;
+
+                self._refresh()
+            });
+
+        },
+        _refresh: function() {
+            console.log('_refresh')
+            $(this.element).val(JSON.stringify(this.options.source_data))
+            // trigger a callback/event
+            this._trigger( "change" );
+        },
+        add_item: function (key, value) {
+            var self = this;
+            if (value instanceof Array) {
+                // if we pass in an array it implies that the key is an array
+                self.form_json_data[key] = self.form_json_data[key] || []
+    console.log(self.form_json_data[key])
+            } else {
+                this.form_json_data[key] = value;
+            }
+        },
+        remove_item: function (key, value) {
+            
+        }
+    });
+});
--- glynt/apps/transact/static/transact/js/jquery.plugin.region_clone.js
+++ glynt/apps/transact/static/transact/js/jquery.plugin.region_clone.js
@@ -17,9 +17,15 @@ $(function() {
 				html: 'x'
 			})
 		},
+		region_key: null,
+		element_data: {},
+		form_json_data: {},
 		// the constructor
 		_create: function() {
 			var self = this;
+
+			this.element_data = this.element.data();
+			this.region_key = this.element_data.regionName;
 
 			this.cloneable_html = this.element.html();
 			this.btn_remove = this.options.btn_remove.clone();
@@ -30,6 +36,37 @@ $(function() {
 			this._on( this._btn_add_another, {
 				click: "add_region"
 			});
+
+			this._listen();
+		},
+		_listen: function () {
+			var self = this;
+            
+			/**
+			* Assign change events to each input item 
+			* in the cloned region
+			*/
+			$.each($(self.cloneable_html).find('input, select, checkbox, radio'), function (i, elem) {
+				self.add_element(elem, false)
+			});
+        
+	        /**
+	        * Handle the LOADED_form_json_data Event
+	        **/
+	        $(document).on("LOADED_form_json_data", function (event) {
+	            self.form_json_data = event.form_json_data;
+	            self.ensure_key(self.form_json_data)
+	            self._refresh();
+	        });
+		},
+        _refresh: function() {
+        	var self = this;
+            $.each(self.form_json_data[self.region_key], function (i, item) {
+            	console.log('ensure that the tem is represented in the html')
+            })
+        },
+		ensure_key: function (form_json_data) {
+			form_json_data[this.region_key] = form_json_data[this.region_key] || {}
 		},
 		btn_add_another: function () {
 			var btn_add_another_id = '{num_elements}_add_another'.assign({'num_elements': this.num_elements})
@@ -39,11 +76,38 @@ $(function() {
 							'label': this.options.label
 					}));
 		},
+		add_element: function (elem, make_new) {
+			elem = $(elem);
+			var self = this;
+			var make_new = (make_new == undefined);
+
+			/**
+			* Dont modify the base cloneable field
+			*/
+			if ( make_new === true ) {
+				// append the current num to the cloned id name
+				var new_id = '{id}-{current_num}'.assign({'id': elem.prop('id'), 'current_num': self.num_elements})
+				var new_name = '{name}-{current_num}'.assign({'name': elem.prop('name'), 'current_num': self.num_elements})
+
+				// update the id and name props
+				elem.prop('id', new_id);
+				elem.prop('name', new_name);
+			}
+
+			elem.on('change', function (event) {
+                $.event.trigger({
+                    type: "MODIFIED_CLONED_REGION_form_json_data",
+                    cloned_region_key: self.region_key,
+                    instance: $(this),
+                });
+			});
+		},
 		add_region: function ( event ) {
 			event.preventDefault()
 			var self = this;
 
 			var cloneable_html = $(this.cloneable_html).clone();
+
 			var legend = $(cloneable_html[0]);
 			var btn_remove = $(this.btn_remove.clone())
 
@@ -65,6 +129,19 @@ $(function() {
 				'class': 'cloned-region',
 				'html': cloneable_html
 			});
+
+			/**
+			* Assign change events to each input item 
+			* in the cloned region
+			*/
+			$.each(cloned_html.find('input, select, checkbox, radio'), function (i, elem) {
+				self.add_element(elem)
+			});
+
+			/**
+			* Add the cloned HTML to the document
+			* -----------------------------------
+			*/
 			this._btn_add_another.before(cloned_html);
 
 			/**
@@ -72,7 +149,7 @@ $(function() {
 			* To allow for custom events to be assigned
 			*/
 			$.event.trigger({
-				type: "region_clone_add",
+				type: "ADD_CLONED_REGION_form_json_data",
 				cloned_region: cloned_html
 			});
 
@@ -84,8 +161,9 @@ $(function() {
 				event.stopPropagation();
 				self.num_elements -= 1;
 				$.event.trigger({
-					type: "region_clone_remove",
-					cloned_region: $(this).closest('.cloned-region')
+					type: "REMOVE_CLONED_REGION_form_json_data",
+					cloned_region: $(this).closest('.cloned-region'),
+					cloned_region_key: self.region_key
 				});
 				$(this).closest('.cloned-region').remove();
 			});
