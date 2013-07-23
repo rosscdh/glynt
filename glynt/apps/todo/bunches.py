4fdcf938974cb54ba60523f50f2ff29674b65355
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-23 16:05:00 +0200

    removed unused duplicated class

--- glynt/apps/todo/bunches.py
+++ glynt/apps/todo/bunches.py
@@ -57,37 +57,3 @@ class BaseToDoBunch(Bunch):
             t = Template(todo.name)
             todo.name = t.render(Context(context))
             todo.save(update_fields=['name'])
-
-# class BaseToDoBunch(Bunch):
-#     """ Sets up the todos per transaction + attachments etc """
-#     user = None
-#     project = None
-#     transaction_slug = None
-#     required_todo_items = [] # list of todo tuples (name,description,attachements_list) to create. i.e. [('My ToDo', 'Is kinda cool', [FileObject1, FileObject2])]
-#     todos = []
-
-#     def __init__(self, user, project=None, *args, **kwargs):
-#         self.user = user
-#         self.project = project
-
-#         self.transaction_todos()
-
-#         super(BaseToDoBunch, self).__init__(*args, **kwargs)
-
-#     @property
-#     def transaction_todos(self):
-#         if not self.todos: # python [] epty list evaluates as False
-#             # if we dont already have todos then setup a list
-#             for name, description, attachments in self.required_todo_items:
-#                 self.todos.push( ToDo( user=self.user, \
-#                             project=self.project \
-#                             name=self.project \
-#                             description=self.project )
-#                 )
-#         return self.todos
-
-
-
-# class SeedFundingBunch(BaseToDoBunch):
-#     transaction_slug = 'seed-funding'
-#     required_todo_items = [('','', [])]
