a8e4493faf2565faf561ca9c46e9870e5733620f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-29 14:46:23 +0200

    Corrected import for renamed Comment -> Discussion view

--- glynt/apps/todo/urls.py
+++ glynt/apps/todo/urls.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
-from .views import (ToDoDetailView, ToDoCommentView, ToDoEditView, ToDoAttachmentView, ToDoAssignView,)
+from .views import (ToDoDetailView, ToDoDiscussionView, ToDoEditView, ToDoAttachmentView, ToDoAssignView,)
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/discussion/$', login_required(ToDoCommentView.as_view()), name='discussion'),
+    url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/discussion/$', login_required(ToDoDiscussionView.as_view()), name='discussion'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/edit/$', login_required(ToDoEditView.as_view()), name='edit'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/attachment/$', login_required(ToDoAttachmentView.as_view()), name='attachment'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/assign/$', login_required(ToDoAssignView.as_view()), name='assign'),
