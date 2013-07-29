36087c4abc687a15ba3226b0e6d5a01db95bcb4b
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-29 14:58:01 +0200

    Setting up of discussion view

--- glynt/apps/todo/urls.py
+++ glynt/apps/todo/urls.py
@@ -6,10 +6,10 @@ from .views import (ToDoDetailView, ToDoDiscussionView, ToDoEditView, ToDoAttach
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/discussion/$', login_required(ToDoDiscussionView.as_view()), name='discussion'),
+    url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/discussion/$', login_required(ToDoDiscussionView.as_view()), name='discuss'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/edit/$', login_required(ToDoEditView.as_view()), name='edit'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/attachment/$', login_required(ToDoAttachmentView.as_view()), name='attachment'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/assign/$', login_required(ToDoAssignView.as_view()), name='assign'),
-    url(r'^(?P<project_uuid>.+)/(?P<slug>.+)$', login_required(ToDoDetailView.as_view()), name='todo'),
+    url(r'^(?P<project_uuid>.+)/(?P<slug>.+)$', login_required(ToDoDetailView.as_view()), name='item'),
     #url(r'^$', login_required(MyToDoListView.as_view()), name='default'),
 )
