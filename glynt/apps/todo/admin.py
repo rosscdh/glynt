7d3829b45b2ed54e41a093cbd88c77955d3b5d9a
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-30 16:01:22 +0200

    Added attachment to admin

--- glynt/apps/todo/admin.py
+++ glynt/apps/todo/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 from django.contrib import admin
 
-from models import ToDo
+from models import ToDo, Attachment
 
 
-admin.site.register([ToDo])
+admin.site.register([ToDo, Attachment])
