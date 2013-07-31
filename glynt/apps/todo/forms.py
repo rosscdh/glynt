4a77a2003500742cfe6466f643583b4595503c54
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-31 11:39:35 +0200

    Getting FPFile working

--- glynt/apps/todo/forms.py
+++ glynt/apps/todo/forms.py
@@ -13,17 +13,19 @@ from .models import ToDo, Attachment
 from django_filepicker.forms import FPFileField
 
 
+FILEPICKER_API_KEY = getattr(settings, 'FILEPICKER_API_KEY', None)
+if FILEPICKER_API_KEY is None:
+    raise 'You must specify a FILEPICKER_API_KEY in your local_settings.py'
+
+
 class AttachmentForm(forms.ModelForm):
     project = forms.IntegerField(widget=forms.HiddenInput)
-
-
+    attachment = FPFileField(label="Company", help_text='', apikey=FILEPICKER_API_KEY, additional_params={'data-fp-button-text' : 'Upload attachment', 'data-fp-button-class':'btn btn-primary'})
 
     class Meta:
         model = Attachment
-        # @ross - i can;t get this bitch working. Have put the settings in loca_settings 
-        #args = {}
-        #kwargs = { 'apikey': 'A4Ly2eCpkR72XZVBKwJ06z', 'additional_params':{'data-fp-button-text' : 'Upload attachment', 'data-fp-button-class':'btn btn-primary' } }
-        #attachment = forms.CharField(label="Company", help_text='', widget=FPFileField(*args, **kwargs))
+
+
 class ToDoForm(forms.ModelForm):
     class Meta:
         model = ToDo
