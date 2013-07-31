2ac01ad9b7797f4ffbd1c8a84bfcd88e5dba3253
Ross Crawford <sendrossemail@gmail.com>
2013-07-31 19:45:49 +0200

    Added logging, and made session generation customizable

--- glynt/apps/todo/services.py
+++ glynt/apps/todo/services.py
@@ -20,13 +20,18 @@ class CrocdocAttachmentService(object):
     attachment = None
     session = None
     def __init__(self, attachment, *args, **kwargs):
+        logger.info('Init CrocdocAttachmentService.__init__ for attachment: {pk}'.format(pk=attachment.pk))
         self.attachment = attachment
 
     @property
     def uuid(self):
         if self.attachment.crocdoc_uuid is None:
-
-            uuid = self.upload_document()
+            try:
+                uuid = self.upload_document()
+                logger.info('CrocdocAttachmentService.uuid: {uuid}'.format(uuid=uuid))
+            except Exception as e:
+                logger.error('CrocdocAttachmentService.uuid: Failed to Generate uuid'.format(uuid=uuid))
+                raise e
 
             crocdoc = self.attachment.data.get('crocdoc', {})
             crocdoc['uuid'] = uuid
@@ -38,16 +43,21 @@ class CrocdocAttachmentService(object):
         else:
             return self.attachment.crocdoc_uuid
 
-    def session_key(self):
+    def session_key(self, **kwargs):
         if self.session is None:
-            self.session = crocodoc.session.create(self.uuid)
+            self.session = crocodoc.session.create(self.uuid, **kwargs)
         return self.session
 
     def upload_document(self):
-        return crocodoc.document.upload(url=self.attachment.get_url())
+        url = self.attachment.get_url()
+        logger.info('Upload file to crocdoc: {url}'.format(url=url))
+        return crocodoc.document.upload(url=url)
 
     def view_url(self):
-        return 'https://crocodoc.com/view/{session_key}'.format(session_key=self.session_key())
+        url = 'https://crocodoc.com/view/{session_key}'.format(session_key=self.session_key())
+        logger.info('provide crocdoc view_url: {url}'.format(url=url))
+        return url
 
     def process(self):
+        logger.info('Start CrocdocAttachmentService.process')
         return self.uuid
