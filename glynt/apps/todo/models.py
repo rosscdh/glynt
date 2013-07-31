ac148e769dd300048815f9a97b33402d61f989c5
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-31 14:20:15 +0200

    made todo optional on attachment

--- glynt/apps/todo/models.py
+++ glynt/apps/todo/models.py
@@ -70,7 +70,7 @@ class ToDo(models.Model):
 class Attachment(models.Model):
     attachment = FPFileField(upload_to=_attachment_upload_file, additional_params=None)
     project = models.ForeignKey(Project, related_name='attachments')
-    todo = models.ForeignKey(ToDo, related_name='attachments')
+    todo = models.ForeignKey(ToDo, blank=True, null=True, related_name='attachments')
     data = JSONField(default={})
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True, db_index=True)
 
