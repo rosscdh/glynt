4586486f41acbcd59d8b8f10d3920fb3ccb2f257
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-07-31 15:11:20 +0200

    Added unix timestamp to dehuydrate

--- glynt/apps/todo/api.py
+++ glynt/apps/todo/api.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 
 from tastypie import fields
 from tastypie.authorization import Authorization
+from tastypie.resources import ALL
 
 from glynt.apps.api import BaseApiModelResource
 from glynt.apps.todo.models import ToDo, Attachment
+
+import time
 
 
 class UserToDoCountResource(BaseApiModelResource):
@@ -51,8 +54,13 @@ class AttachmentResource(BaseApiModelResource):
         authorization = Authorization()
         resource_name = 'todo/attachment'
         list_allowed_methods = ['get', 'post', 'delete']
+        filtering = {
+            'project': ALL,
+            'todo': ALL,
+        }
 
     def dehydrate(self, bundle):
         bundle.data.pop('data')
         bundle.data['data'] = bundle.obj.data
+        bundle.data['date_created_unix'] = time.mktime(bundle.obj.date_created.timetuple())
         return bundle
