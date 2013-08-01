a889ca640d230eeb54e7c9c4c784d3685169808f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-01 14:33:10 +0200

    Added AWS_FILESTORE_BUCKET, and developed delete functionality that removes files from (s3, inkfilepicker and crocdoc)

--- glynt/apps/todo/services.py
+++ glynt/apps/todo/services.py
@@ -4,14 +4,32 @@ Service that abstract the various creation processes todos
 """
 from django.conf import settings
 
+from boto.s3.connection import S3Connection
+
 import logging
 logger = logging.getLogger('lawpal.services')
+
+import crocodoc
+import requests
+
+FILEPICKER_API_KEY = getattr(settings, 'FILEPICKER_API_KEY', None)
+if FILEPICKER_API_KEY is None:
+    raise Exception('You must specify a FILEPICKER_API_KEY in your local_settings.py')
 
 CROCDOC_API_KEY = getattr(settings, 'CROCDOC_API_KEY', None)
 if CROCDOC_API_KEY is None:
     raise Exception("You must specify a CROCDOC_API_KEY in your local_settings.py")
 
-import crocodoc
+AWS_ACCESS_KEY_ID = getattr(settings, 'AWS_ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = getattr(settings, 'AWS_SECRET_ACCESS_KEY', None)
+AWS_FILESTORE_BUCKET = getattr(settings, 'AWS_FILESTORE_BUCKET', None)
+
+if AWS_ACCESS_KEY_ID is None:
+    raise Exception("You must specify a AWS_ACCESS_KEY_ID in your local_settings.py")
+if AWS_SECRET_ACCESS_KEY is None:
+    raise Exception("You must specify a AWS_SECRET_ACCESS_KEY in your local_settings.py")
+if AWS_FILESTORE_BUCKET is None:
+    raise Exception("You must specify a AWS_FILESTORE_BUCKET in your local_settings.py")
 
 crocodoc.api_token = CROCDOC_API_KEY
 
@@ -19,6 +37,7 @@ crocodoc.api_token = CROCDOC_API_KEY
 class CrocdocAttachmentService(object):
     attachment = None
     session = None
+
     def __init__(self, attachment, *args, **kwargs):
         logger.info('Init CrocdocAttachmentService.__init__ for attachment: {pk}'.format(pk=attachment.pk))
         self.attachment = attachment
@@ -60,6 +79,43 @@ class CrocdocAttachmentService(object):
         logger.info('provide crocdoc view_url: {url}'.format(url=url))
         return url
 
+    def remove(self):
+        # delete from crocdoc based on uuid
+        deleted = crocodoc.document.delete(self.attachment.crocdoc_uuid)
+        if deleted:
+            logger.info('Deleted crocdoc file: {pk} - {uuid}'.format(pk=self.attachment.pk, uuid=self.attachment.crocdoc_uuid))
+        else:
+            logger.error('Could not Delete crocdoc file: {pk} - {uuid}'.format(pk=self.attachment.pk, uuid=self.attachment.crocdoc_uuid))
+
     def process(self):
         logger.info('Start CrocdocAttachmentService.process')
         return self.uuid
+
+
+class InkFilePickerAttachmentService(object):
+    def __init__(self, attachment, *args, **kwargs):
+        logger.info('Init InkFilePickerAttachmentService.__init__ for attachment: {pk}'.format(pk=attachment.pk))
+        self.attachment = attachment
+
+    def remove(self):
+        try:
+            self.remove_from_inkfilepicker()
+        except Exception as e:
+            logger.error('InkFilePickerAttachmentService: Could not Delete inkfilepicker file: {pk} Error: {error}'.format(pk=self.attachment.pk, error=e))
+
+        try:
+            self.remove_from_s3()
+        except Exception as e:
+            logger.error('InkFilePickerAttachmentService: Could not Delete s3 file: {pk} Error: {error}'.format(pk=self.attachment.pk, error=e))
+
+    def remove_from_inkfilepicker(self):
+        url = self.attachment.inkfilepicker_url
+        if url:
+            r = requests.delete(url, params={'key': FILEPICKER_API_KEY})
+            logger.info('InkFilePickerAttachmentService.remove_from_inkfilepicker for attachment: {pk} - to: {url} response ({status_code}): {response}'.format(pk=self.attachment.pk, url=r.url, response=r, status_code=r.status_code))
+
+    def remove_from_s3(self):
+        conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+        bucket = conn.get_bucket(AWS_FILESTORE_BUCKET)
+        r = bucket.delete_key(self.attachment.s3_key)
+        logger.info('InkFilePickerAttachmentService.remove_from_s3 for attachment: {pk} - to: {s3_key}, Response: {r}'.format(pk=self.attachment.pk, s3_key=self.attachment.s3_key, r=r))
--- glynt/apps/todo/signals.py
+++ glynt/apps/todo/signals.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """ Set of signals to handle when comments are posted and assigning notifications to the user """
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 
 # from notifications import notify
 # from notifications.models import Notification
 
 from .models import Attachment
-from .services import CrocdocAttachmentService
+from .services import CrocdocAttachmentService, InkFilePickerAttachmentService
 
 import logging
 logger = logging.getLogger('django.request')
@@ -16,11 +16,27 @@ logger = logging.getLogger('django.request')
 @receiver(post_save, sender=Attachment, dispatch_uid='todo.attachment.created')
 def on_attachment_created(sender, **kwargs):
     """
-    Handle new Project
+    Handle Creation of attachments
     """
     is_new = kwargs.get('created')
     attachment = kwargs.get('instance')
 
     if attachment:
-        upload_service = CrocdocAttachmentService(attachment=attachment)
-        upload_service.process()
+        crocdoc_service = CrocdocAttachmentService(attachment=attachment)
+        crocdoc_service.process()
+
+
+@receiver(post_delete, sender=Attachment, dispatch_uid='todo.attachment.deleted')
+def on_attachment_deleted(sender, **kwargs):
+    """
+    Handle Deletions of attachments
+    """
+    is_new = kwargs.get('created')
+    attachment = kwargs.get('instance')
+
+    if attachment:
+        crocdoc_service = CrocdocAttachmentService(attachment=attachment)
+        crocdoc_service.remove()
+
+        inkfilepicker_service = InkFilePickerAttachmentService(attachment=attachment)
+        inkfilepicker_service.remove()
