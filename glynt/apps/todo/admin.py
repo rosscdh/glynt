5653009b0e3519fef678499112ad0d3f09ba15fe
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-01 12:45:13 +0200

    made attachment admin interface nicer

--- glynt/apps/todo/admin.py
+++ glynt/apps/todo/admin.py
@@ -3,5 +3,27 @@ from django.contrib import admin
 
 from models import ToDo, Attachment
 
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'slug')
+    search_fields = ('name', 'category', 'slug')
 
-admin.site.register([ToDo, Attachment])
+    def queryset (self, request):
+        qs = ToDo.objects.prefetch_related('project').all()
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'project', 'filename', 'mimetype', 'size', 'date_created')
+    search_fields = ('uuid',)
+
+    def queryset (self, request):
+        qs = Attachment.objects.prefetch_related('project').all()
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+admin.site.register(ToDo, ToDoAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
