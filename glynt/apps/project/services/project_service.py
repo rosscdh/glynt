96e26c26c35b62904012315dd47fec379f54562d
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-13 16:08:06 +0200

    added lawyer_projects to get access to the ProjectLawyers object and properties

--- glynt/apps/project/services/project_service.py
+++ glynt/apps/project/services/project_service.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from glynt.apps.project.models import Project
+from glynt.apps.project.models import Project, ProjectLawyer
+from glynt.apps.project import PROJECT_STATUS
 
 
 class VisibleProjectsService(object):
@@ -17,13 +18,17 @@ class VisibleProjectsService(object):
         except IndexError:
             return None
 
+    def lawyer_projects(self, **kwargs):
+        return ProjectLawyer.objects.filter(lawyer=self.user.lawyer_profile).filter(**kwargs)
+
     def get(self, **kwargs):
         if self.user.profile.is_customer and 'customer' not in kwargs:
             kwargs.update({'customer': self.user.customer_profile})
 
         if self.projects is None:
             if self.user:
-                self.projects = Project.objects.new(**kwargs).order_by('id') | Project.objects.open(**kwargs).order_by('id')
+                #self.projects = Project.objects.new(**kwargs).order_by('id') | Project.objects.open(**kwargs).order_by('id')
+                self.projects = Project.objects.filter(status__in=[PROJECT_STATUS.open, PROJECT_STATUS.new], **kwargs)
                 self.projects = self.projects.select_related('company', 'customer__user', 'transactions')
 
         return self.projects
