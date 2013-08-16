ec77621c55831189e7e49fccff0ff8de15dca398
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-16 08:37:31 +0200

    removed redundant service

--- glynt/apps/services/email/actions.py
+++ glynt/apps/services/email/actions.py
@@ -8,16 +8,3 @@ class NewActionEmailService(BaseEmailService):
     Inform the relevant parties of a new action
     """
     email_template = 'new.action'
-
-
-# class TodoCommentEmailService(BaseEmailService):
-#     """
-#     Email Service
-#     Inform the relevant parties of a new comment
-#     """
-#     email_template = 'new.action'
-
-#     @property
-#     def message(self):
-#         return '{from_name} commented on a checklist item.\n\n' \
-#                '{comment}\n'.format(from_name=self.from_name, comment=self.context.get('comment').comment)
