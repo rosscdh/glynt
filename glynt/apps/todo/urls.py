44eb244f801f8fdc045991b2215725a277d10be9
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-21 10:17:22 +0200

    Fixing object reference in the html

--- glynt/apps/todo/urls.py
+++ glynt/apps/todo/urls.py
@@ -7,14 +7,9 @@ from .views import (ToDoDetailView, ToDoDiscussionView, ToDoEditView,\
 
 
 urlpatterns = patterns('',
-    # url(r'^feedback/(?P<pk>\d+)/$', login_required(FeedbackRequestJSONView.as_view()), name='feedback_request'),
     url(r'^attachment/(?P<pk>\d+)/$', login_required(AttachmentView.as_view()), name='attachment'),
-
     url(r'^(?P<project_uuid>.+)/create/$', login_required(ToDoCreateView.as_view()), name='create'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/discussion/$', login_required(ToDoDiscussionView.as_view()), name='discuss'),    
-    # url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/assign/$', login_required(ToDoAssignView.as_view()), name='assign'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)/edit/$', login_required(ToDoEditView.as_view()), name='edit'),
     url(r'^(?P<project_uuid>.+)/(?P<slug>.+)$', login_required(ToDoDetailView.as_view()), name='item'),
-
-    #url(r'^$', login_required(MyToDoListView.as_view()), name='default'),
 )
