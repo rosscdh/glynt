dc7bb59eed3f6d27ab41cf21eaf5ba3fc1904998
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-22 12:58:08 +0200

    added lawyer checklist casper test

---
+++ glynt/apps/dashboard/casper-tests/checklist-lawyer.js
@@ -0,0 +1,59 @@
+casper.test.comment(casper.cli.options.test_label);
+
+var helper = require(casper.cli.options.casper_helper_js_path);
+
+/**
+* Test for the Checklist Url like: /dashboard/10a601ce31c24133939f3eced7376097/checklist/
+*/
+helper.scenario(casper.cli.options.url,
+    function() {
+        casper.test.comment('Test Page General Access and Title');
+        //this.echo(this.getHTML());
+        this.echo(this.getTitle());
+        this.test.assertHttpStatus(200);
+
+        this.test.assertMatch(this.getTitle(), /^Checklist for\:/ig, 'Title contains Checklist for:');
+        // --
+    },
+    function() {
+        casper.test.comment('Test checklist categories exist');
+
+        this.test.assertExists('ul#checklist-categories')
+        this.test.assertExists('ul#checklist-categories li')
+    },
+    function() {
+        casper.test.comment('Test checklist items exist');
+
+        this.test.assertExists('tr.item')
+        this.test.assertExists('button.create-item')
+
+        casper.test.comment('Test checklist items edit button exists and is visible');
+        this.test.assertExists('tr.item a.item-edit')
+        this.test.assertVisible('tr.item a.item-edit')
+
+        casper.test.comment('Test checklist items delete button exists and is not visible');
+        this.test.assertExists('tr.item a.item-delete')
+        this.test.assertNotVisible('tr.item a.item-delete')
+        
+    },
+    function() {
+        var self = this;
+        casper.test.comment('Test edit item shows modal');
+
+        this.evaluate(function() {
+            var selector = $('tr.item a.item-edit:first');
+            selector.trigger('click');
+        });
+
+        casper.waitFor(function check() {
+            return this.evaluate(function() {
+                return $('#modal-checklist-item').length > 0;
+            });
+        }, function then() {
+            this.capture('/tmp/modal_click.png')
+            this.captureSelector('/tmp/yoursitelist.png', 'div#modal-checklist-item');
+        });
+    }
+);
+
+helper.run();
