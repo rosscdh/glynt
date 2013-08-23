02932c128fb7c395154bf1e4f1b980faa25f692e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-23 16:15:16 +0200

    Getting signals all imported correctly. Updating signals to send checklist item name and project in subject of all emails

--- glynt/apps/todo/models.py
+++ glynt/apps/todo/models.py
@@ -171,4 +171,9 @@ class FeedbackRequest(models.Model):
 """
 import signals
 """
-from glynt.apps.todo.signals import (on_attachment_created, on_attachment_deleted, on_comment_created)
+from glynt.apps.todo.signals import (on_attachment_created, on_attachment_deleted, \
+                                        on_comment_created, feedbackrequest_created, \
+                                        feedbackrequest_status_change, projectlawyer_assigned, \
+                                        projectlawyer_deleted, todo_item_status_change, \
+                                        on_action_created
+                                    )
