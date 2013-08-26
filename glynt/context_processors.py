50e9cb038af81caedb40f2592c6dc601191ba1b2
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-08-26 16:13:17 +0200

    patched context_processor to make use of the middleware appended request.projects and request.project

--- glynt/context_processors.py
+++ glynt/context_processors.py
@@ -2,21 +2,18 @@
 import os
 from django.conf import settings
 
-from glynt.apps.project.models import Project, ProjectLawyer
-
-
 
 def project_info(request):
     PROJECT_NAME = getattr(settings, 'PROJECT_NAME', 'LawPal')
     return {
-      'PROJECT_NAME': PROJECT_NAME
+        'PROJECT_NAME': PROJECT_NAME
     }
 
 
 def project_environment(request):
     PROJECT_ENVIRONMENT = getattr(settings, 'PROJECT_ENVIRONMENT', 'prod')
     return {
-      'PROJECT_ENVIRONMENT': PROJECT_ENVIRONMENT
+        'PROJECT_ENVIRONMENT': PROJECT_ENVIRONMENT
     }
 
 
@@ -35,31 +32,26 @@ def USE_THREADEDCOMMENTS(request):
 
 def PUSHER_DATA(request):
     return {
-      'PUSHER_APP_ID': getattr(settings, 'PUSHER_APP_ID'),
-      'PUSHER_KEY': getattr(settings, 'PUSHER_KEY')
+        'PUSHER_APP_ID': getattr(settings, 'PUSHER_APP_ID'),
+        'PUSHER_KEY': getattr(settings, 'PUSHER_KEY')
     }
+
 
 def user_projects(request):
     """
     Ensures the users projects and project are in the context at all times
+    the request.projects and request.project objects are passed in via the
+    project.services.project_service:VisibleProjectsService
     """
     projects = []
     project = None
 
-    if request.user.is_authenticated() and not request.user.is_staff and not request.user.is_superuser:
+    try:
+        projects = request.projects
+        project = request.project
+    except:
+        pass
 
-        if request.user.profile.is_lawyer:
-            projects = [join.project for join in ProjectLawyer.objects.assigned(lawyer=request.user.lawyer_profile)]
-            try:
-                project = projects[0]
-            except IndexError:
-                pass
-        elif request.user.profile.is_customer:
-            projects = Project.objects.current(customer=request.user.customer_profile)
-            try:
-                project = projects[0]
-            except IndexError:
-                pass
     return {
         'projects': projects,
         'project': project,
