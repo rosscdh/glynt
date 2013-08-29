80ed35b9d6dd2173c96d6f43b0d712a0b22fa43b
Ross Crawford <sendrossemail@gmail.com>
2013-08-29 15:14:08 +0200

    Added LawyerLiteProfileView

--- glynt/apps/lawyer/urls.py
+++ glynt/apps/lawyer/urls.py
@@ -4,7 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 from glynt.apps.lawyer.forms import LawyerProfileSetupForm
-from glynt.apps.lawyer.views import LawyerListView, LawyerProfileSetupView, LawyerProfileView
+from glynt.apps.lawyer.views import (LawyerListView, LawyerLiteProfileView, 
+										LawyerProfileSetupView, LawyerProfileView,)
 
 
 urlpatterns = patterns('',
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^invite/$', login_required(TemplateView.as_view(template_name='lawyer/invite.html')), name='invite'),
 
     url(r'^welcome/$', login_required(TemplateView.as_view(template_name='lawyer/welcome.html')), name='welcome'),
+    url(r'^(?P<slug>.+)/lite/$', LawyerLiteProfileView.as_view(), name='profile_lite'),
     url(r'^(?P<slug>.+)/$', LawyerProfileView.as_view(), name='profile'),
     url(r'^$', LawyerListView.as_view(), name='list'),
 )
