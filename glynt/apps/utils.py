c694b0c62bdaaf80a92428351d8a1c5f96d14823
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-05 09:33:27 +0200

    fixing? unicode errors

--- glynt/apps/utils.py
+++ glynt/apps/utils.py
@@ -34,8 +34,8 @@ def generate_unique_slug(instance=None):
     """ Generate the unique slug for a model instance """
     if instance is not None:
         pk = instance.pk if hasattr(instance, 'pk') and type(instance.pk) is not None else random.random()
-        hash_val = u'%s-%s-%s' % (instance.__class__.__name__, pk, datetime.datetime.utcnow())
-
+        hash_val = '%s-%s-%s' % (instance.__class__.__name__, pk, datetime.datetime.utcnow())
+        import pdb;pdb.set_trace()
         return shortuuid.uuid(name=hash_val)
     else:
         return shortuuid.uuid()
