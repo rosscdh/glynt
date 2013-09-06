ff126b73db787b578e2e5be6bfa96a04d8ca9601
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-06 08:38:35 +0200

    removed django-nose as it is quite buggy and does not find all the tests

--- glynt/test_settings.py
+++ glynt/test_settings.py
@@ -4,6 +4,9 @@ from settings import *
 
 import hashlib
 import random
+import logging
+
+logging.disable(logging.CRITICAL)
 
 # Custom test runner for this project
 TEST_RUNNER = 'glynt.test_runner.GlyntAppTestRunner'
@@ -11,83 +14,15 @@ TEST_RUNNER = 'glynt.test_runner.GlyntAppTestRunner'
 PROJECT_ENVIRONMENT = 'test'
 
 INSTALLED_APPS = INSTALLED_APPS + (
-    'django_nose',
     'casper',
 )
 
-def get_testable_apps():
-    return ' '.join([app for app in PROJECT_APPS])
-
-def get_excluded_apps():
-    return ','.join([app for app in DJANGO_APPS + HELPER_APPS])
-
-NOSE_ARGS = [
-    # '--with-coverage',
-    # '--all-modules'
-    #'--exclude={exclude}'.format(exclude=get_excluded_apps()),
-    '{tests}'.format(tests=get_testable_apps()),
-]
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.test': {
-            'handlers': ['null'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'django.test.behave': {
-            'handlers': ['null'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['null'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'lawpal.services': {
-            'handlers': ['null'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'lawpal.graph': {
-            'handlers': ['null'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'lawpal.commands': {
-            'handlers': ['null'],
-            'level': 'INFO',
-            'propagate': True,
-        }
-    }
-}
+
 
 def AutoSlugFieldGenerator():
     hash_val = '{r}'.format(r=random.random())
