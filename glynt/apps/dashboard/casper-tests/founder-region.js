015fae9f4b0e713a3baa3e4585ac43125a8b4f22
Jamie Connolly <jamie@lawpal.com>
2013-09-06 15:19:37 +0100

    Add in the tests for the founder region removal

--- glynt/apps/dashboard/casper-tests/founder-region.js
+++ glynt/apps/dashboard/casper-tests/founder-region.js
@@ -3,25 +3,42 @@ casper.test.comment(casper.cli.options.test_label);
 var helper = require(casper.cli.options.casper_helper_js_path);
 
 /**
- * Test to see if the contact us modal displays.
+ * Test to see if the founder region works.
  */
 helper.scenario(casper.cli.options.url,
     function() {
         this.test.comment('Test the addition of a cloned founder region');
 
-        // test the add founder block works
         this.test.assertElementCount('.founder-group', 1);
-        casper.test.assertElementCount('button.delete-cloned-region', 0);
+        this.test.assertElementCount('button.delete-cloned-region', 0);
 
         this.click('button#btn_add_another');
-        casper.test.assertElementCount('.founder-group', 2);
-        casper.test.assertElementCount('button.delete-cloned-region', 1);
+        this.test.assertElementCount('.founder-group', 2);
+        this.test.assertElementCount('button.delete-cloned-region', 1);
 
         this.click('button#btn_add_another');
-        casper.test.assertElementCount('.founder-group', 3);
-        casper.test.assertElementCount('button.delete-cloned-region', 2);
+        this.test.assertElementCount('.founder-group', 3);
+        this.test.assertElementCount('button.delete-cloned-region', 2);
+    }
+);
 
+helper.scenario(casper.cli.options.url,
+    function() {
         this.test.comment('Test the removal of a cloned founder region');
+
+        this.click('button#btn_add_another');
+        this.click('button#btn_add_another');
+
+        this.test.assertElementCount('.founder-group', 3);
+        this.test.assertElementCount('button.delete-cloned-region', 2);
+
+        this.click('button.delete-cloned-region:first-child');
+        this.test.assertElementCount('.founder-group', 2);
+        this.test.assertElementCount('button.delete-cloned-region', 1);
+
+        this.click('button.delete-cloned-region:first-child');
+        this.test.assertElementCount('.founder-group', 1);
+        this.test.assertElementCount('button.delete-cloned-region', 0);
     }
 );
 
