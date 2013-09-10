dcca58e458bf73efc8b259b4909326a6b0272aab
Jamie Connolly <jamie@lawpal.com>
2013-09-10 14:41:01 +0100

    Moved the casper tests.

--- glynt/apps/dashboard/tests.py
+++ glynt/apps/dashboard/tests.py
@@ -100,13 +100,3 @@ class ChecklistLawyerTest(BaseLawyerCustomerProjectCaseMixin, PyQueryMixin):
         url = reverse('dashboard:checklist', kwargs={'uuid': self.project.uuid})
         self.assertTrue(self.load_casper_file(js_file='checklist-lawyer.js', test_label='Test the Checklist View for a Lawyer', url=url))
         # from nose.tools import set_trace; set_trace()
-
-
-class TransactionBuilderTest(BaseLawyerCustomerProjectCaseMixin, PyQueryMixin):
-    test_path = os.path.dirname(__file__)
-
-    def test_remove_founder_region_js(self):
-        self.client.login(username=self.customer_user.username, password=self.password)
-
-        url = reverse('transact:builder', kwargs={ 'project_uuid': self.project.uuid, 'tx_range': 'CS', 'step': 1 })
-        self.assertTrue(self.load_casper_file(js_file='founder-region.js', test_label='Test the founder region', url=url))
--- glynt/apps/transact/tests/__init__.py
+++ glynt/apps/transact/tests/__init__.py
@@ -1,1 +1,3 @@
 # coding: utf-8
+from test_bunches import *
+from test_transaction_builder import *
