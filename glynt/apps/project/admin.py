e5f1cff1618143e48f033e894e415d0bfeb5c9d3
Jamie Connolly <jamie@lawpal.com>
2013-09-13 13:16:15 +0100

    Added in the inline view to add lawyers to a project.

--- glynt/apps/project/admin.py
+++ glynt/apps/project/admin.py
@@ -4,17 +4,23 @@ from django.contrib import admin
 from glynt.apps.project.models import Project, ProjectLawyer
 
 
+class ProjectLawyerInline(admin.TabularInline):
+    model = ProjectLawyer
+
+
 class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectLawyerInline]
     list_filter = ['status', 'transactions']
     list_display = ('__unicode__', 'date_created', 'date_modified')
     search_fields = ('company', 'customer', 'lawyers', 'transactions')
 
-    def queryset (self, request):
+    def queryset(self, request):
         qs = Project.objects.select_related('company', 'customer', 'customer__user').all()
         ordering = self.get_ordering(request)
         if ordering:
             qs = qs.order_by(*ordering)
         return qs
+
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register([ProjectLawyer])
