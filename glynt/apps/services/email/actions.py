bfd71b2bde5d6e8a76ea1a59339d9f2ef9722c39
Jamie Connolly <jamie@lawpal.com>
2013-09-17 16:33:04 +0100

    Update the NewActionEmailService to check the can_send property

--- glynt/apps/services/email/actions.py
+++ glynt/apps/services/email/actions.py
@@ -7,4 +7,22 @@ class NewActionEmailService(BaseEmailService):
     Email Service
     Inform the relevant parties of a new action
     """
-    email_template = 'new.action'
+    verb = 'new.action'
+    whitelist_actions = ['new.action']
+
+    def __init__(self, **kwargs):
+        self.verb = kwargs.get('verb', self.verb)
+
+        super(NewActionEmailService, self).__init__(**kwargs)
+
+    @property
+    def can_send(self):
+        return self.verb in self.whitelist_actions
+
+    @property
+    def email_template(self):
+        return self.verb
+
+    def send(self, **kwargs):
+        if self.can_send:
+            super(NewActionEmailService, self).send(**kwargs)
