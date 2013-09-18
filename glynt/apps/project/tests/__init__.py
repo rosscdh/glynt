5a4a66e49c7f466208d8321c9aaaecf2d80df822
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-18 15:52:07 +0200

    Added transaction select tests

--- glynt/apps/project/tests/__init__.py
+++ glynt/apps/project/tests/__init__.py
@@ -2,3 +2,4 @@
 
 from .base import *
 from .service_mixins import *
+from .project_flow import *
---
+++ glynt/apps/project/tests/project_flow.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+from django.core.urlresolvers import reverse
+from glynt.casper import BaseLawyerCustomerProjectCaseMixin, PyQueryMixin
+
+import os
+
+
+class CustomerCreateProjectTest(BaseLawyerCustomerProjectCaseMixin, PyQueryMixin):
+    test_path = os.path.dirname(__file__)
+
+    def test_customer_transact_select_js(self):
+        """
+        Test that the Customer has a list of transaction types to select from
+        """
+        self.client.login(username=self.lawyer_user.username, password=self.password)
+
+        url = reverse('project:create')
+        self.assertTrue(self.load_casper_file(js_file='customer-transact-select.js', test_label='Test a Customer can select transactions', url=url))
