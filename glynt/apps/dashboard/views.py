12f41a8425286111b1361860b50e4d9a472acf55
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-24 10:44:32 +0200

    Fixing the dashboard view for customers lawyers etc

--- glynt/apps/dashboard/views.py
+++ glynt/apps/dashboard/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.http import Http404
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
 
 from bunch import Bunch
 
+from glynt.apps.project.models import Project
 from glynt.apps.project.bunches import ProjectIntakeFormIsCompleteBunch
 from glynt.apps.project import PROJECT_LAWYER_STATUS
 from glynt.apps.todo.views import ToDoCountMixin
@@ -25,6 +27,7 @@ class DashboardView(ToDoCountMixin, TemplateView):
         """ if we are a lawyer, then use the lawyer overview template"""
         if 'uuid' in kwargs:
             self.project_uuid = self.get_project_uuid()
+            self.request.session['current_project_uuid'] = self.project_uuid  # @TODO fix this codesmell encapsulate in nice method
 
         else:
             if request.user.profile.is_lawyer:
@@ -33,12 +36,10 @@ class DashboardView(ToDoCountMixin, TemplateView):
         return super(DashboardView, self).dispatch(request, *args, **kwargs)
 
     def qs_filter(self):
-        project_uuid = self.get_project_uuid()
-
         qs_filter = {}
 
-        if project_uuid is not None:
-            qs_filter = {'uuid': project_uuid}
+        if self.project_uuid is not None:
+            qs_filter = {'uuid': self.project_uuid}
 
         if self.request.user.profile.is_customer:
             qs_filter.update({
@@ -61,34 +62,42 @@ class DashboardView(ToDoCountMixin, TemplateView):
         intake_complete = ProjectIntakeFormIsCompleteBunch(project=self.request.project)
         profile_is_complete = intake_complete.is_valid()
 
-        return Bunch({
+        return {
             'profile_is_complete': profile_is_complete,
-        })
+        }
 
     def get_context_data(self, **kwargs):
+        """
+        is lawyer + no uuid = show lawyer project list
+        is lawyer + has uuid = show project overview
+        is customer + no uuid = show default project overview (customer must have at least 1 project)
+        is customer + has uuid = show default project overview (customer must have at least 1 project)
+        """
         qs_filter = self.qs_filter()
+
+        if self.project_uuid is None:
+            project = self.request.project
+        else:
+            project = get_object_or_404(Project, uuid=self.project_uuid)
+
+        kwargs.update({
+            'project': project,
+        })
+
+        # append counts
+        kwargs.update(self.todo_counts(qs_objects=project.todo_set, project=project))
+
+        if self.request.user.profile.is_customer:
+            kwargs.update(
+                self.customer_context(project=project)
+            )
+
+        elif self.request.user.profile.is_lawyer:
+            kwargs.update(self.lawyer_context())
 
         kwargs.update({
             'PROJECT_LAWYER_STATUS': PROJECT_LAWYER_STATUS,
             'projects': self.request.projects,
-            'project': self.request.project,
         })
-
-        if self.request.project:
-            # append counts
-            kwargs.update(self.todo_counts(qs_objects=self.request.project.todo_set, project=self.request.project))
-
-        if self.request.user.profile.is_customer:
-
-            if self.request.project is None:
-                raise Http404('Project uuid: %s does not exist' % qs_filter.get('uuid'))
-
-            kwargs.update(
-                self.customer_context(project=self.request.project)
-            )
-
-        elif self.request.user.profile.is_lawyer:
-
-            kwargs.update(self.lawyer_context())
 
         return kwargs
