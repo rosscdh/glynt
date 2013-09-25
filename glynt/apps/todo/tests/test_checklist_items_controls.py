9eabc4aa08c6fbee277a70cbf33ed3716613a7cb
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-25 10:25:57 +0200

    removed unused module imports and fixed up spacing (4 spaces not tabs please)

--- glynt/apps/todo/tests/test_checklist_items_controls.py
+++ glynt/apps/todo/tests/test_checklist_items_controls.py
@@ -2,17 +2,16 @@
 """
 """
 from glynt.casper import BaseLawyerCustomerProjectCaseMixin
-from glynt.apps.todo.models import FeedbackRequest
-from glynt.apps.todo import TODO_STATUS, FEEDBACK_STATUS
-from model_mommy import mommy
+from django.core.urlresolvers import reverse
 
 import os
-import httpretty
 
-class ChecklistLawyerDetailControlsTest(BaseLawyerCustomerProjectCaseMixin, PyQueryMixin):
 
-   def test_lawyer_detail_controls_js(self):
-           self.client.login(username=self.lawyer_user.username, password=self.password)
+class ChecklistLawyerDetailControlsTest(BaseLawyerCustomerProjectCaseMixin):
+    test_path = os.path.dirname(__file__)
 
-           url = reverse('todo:item', kwargs={'uuid': self.project.uuid, 'slug':self.todo.slug})
-           self.assertTrue(self.load_casper_file(js_file='todo-item-detail-controls.js', test_label='Test the Checklist Detail....', url=url))
+    def test_lawyer_detail_controls_js(self):
+        self.client.login(username=self.lawyer_user.username, password=self.password)
+
+        url = reverse('todo:item', kwargs={'project_uuid': self.project.uuid, 'slug': self.todo.slug})
+        self.assertTrue(self.load_casper_file(js_file='todo-item-detail-controls.js', test_label='Test the Checklist Detail.', url=url))
