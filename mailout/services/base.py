e4c4e2a274c11423f0e9ad45cc0dcfc7c4c3ee8c
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-26 16:25:45 +0200

    Moved methods into the right parts

--- mailout/services/base.py
+++ mailout/services/base.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-from mailout import ABRIDGE_API_URL
+from mailout import (ABRIDGE_ACCESS_KEY_ID, ABRIDGE_SECRET_ACCESS_KEY,
+                     ABRIDGE_USERNAME, ABRIDGE_PASSWORD, ABRIDGE_API_URL)
+
 from .mixins import UserGetOrCreateMixin
+
+import requests
 
 
 class MailoutConnectionBase(UserGetOrCreateMixin):
     """
     Base Mailout Connection
     """
-    client = None
     base_uri = ABRIDGE_API_URL
+    _access_token = None
 
     def __init__(self, url=None, **kwargs):
         self.base_uri = url if url is not None else self.base_uri
@@ -18,6 +22,51 @@ class MailoutConnectionBase(UserGetOrCreateMixin):
 
         # ensure that we have the current logged in user in abridge
         self.ensure_abridge_user()
+
+    @property
+    def headers(self):
+        return {
+            'Authorization': 'Bearer {access_token}'.format(access_token=self.access_token),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+
+    @property
+    def access_token(self):
+        """
+        Get the all important access_token
+        """
+        # if we dont already have the token
+        if self._access_token is None:
+            # content defined so we can test for response
+            content = {}
+
+            # set std form post headers for the auth token request
+            headers = {
+                'Content-type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+            }
+
+            data = {
+                'client_id': ABRIDGE_ACCESS_KEY_ID,
+                'client_secret': ABRIDGE_SECRET_ACCESS_KEY,
+                'grant_type': 'password',
+                'username': ABRIDGE_USERNAME,
+                'password': ABRIDGE_PASSWORD,
+            }
+
+            # query the abridge api
+            url = self.get_url(path='oauth2/access_token/')
+            r = requests.post(url, data, headers=headers)
+
+            if r.status_code in [200]:
+                content = r.json()
+                self._access_token = content.get('access_token')
+
+            if content.get('access_token', None) is None:
+                raise Exception('Could not obtain access_token: {content}'.format(content=r))
+
+        return self._access_token
 
     def get_url(self, path):
         return '{base}{path}'.format(base=self.base_uri, path=path)
--- mailout/services/mixins.py
+++ mailout/services/mixins.py
@@ -1,60 +1,17 @@
 # -*- coding: utf-8 -*-
-from mailout import (ABRIDGE_ACCESS_KEY_ID, ABRIDGE_SECRET_ACCESS_KEY,
-                     ABRIDGE_USERNAME, ABRIDGE_PASSWORD, ABRIDGE_PROJECT)
+from mailout import ABRIDGE_PROJECT
+
 import requests
 import hashlib
 import json
 
 
 class UserGetOrCreateMixin(object):
-    _access_token = None
+    """
+    The User Mixin that allows us to communicate with Abridge as a user
+    """
     _user = None
     _email_hash = None
-
-    @property
-    def headers(self):
-        return {
-            'Authorization': 'Bearer {access_token}'.format(access_token=self.access_token),
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        }
-
-    @property
-    def access_token(self):
-        """
-        Get the all important access_token
-        """
-        # if we dont already have the token
-        if self._access_token is None:
-            # content defined so we can test for response
-            content = {}
-
-            # set std form post headers for the auth token request
-            headers = {
-                'Content-type': 'application/x-www-form-urlencoded',
-                'Accept': 'application/json',
-            }
-
-            data = {
-                'client_id': ABRIDGE_ACCESS_KEY_ID,
-                'client_secret': ABRIDGE_SECRET_ACCESS_KEY,
-                'grant_type': 'password',
-                'username': ABRIDGE_USERNAME,
-                'password': ABRIDGE_PASSWORD,
-            }
-
-            # query the abridge api
-            url = self.get_url(path='oauth2/access_token/')
-            r = requests.post(url, data, headers=headers)
-
-            if r.status_code in [200]:
-                content = r.json()
-                self._access_token = content.get('access_token')
-
-            if content.get('access_token', None) is None:
-                raise Exception('Could not obtain access_token: {content}'.format(content=r))
-
-        return self._access_token
 
     def ensure_abridge_user(self):
         """
