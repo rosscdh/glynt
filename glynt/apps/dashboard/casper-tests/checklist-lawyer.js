8a42a51d3bc80674da0ee61d473a795573c75ba2
Lee <lee@kudosknowledge.com>
2013-09-27 18:01:57 +1000

    Added move icon and cursor to category list on left hand side of checklist items page. Id you are a lawyer

--- glynt/apps/dashboard/casper-tests/checklist-lawyer.js
+++ glynt/apps/dashboard/casper-tests/checklist-lawyer.js
@@ -174,16 +174,16 @@ helper.scenario(casper.cli.options.url,
         casper.viewport(1024, 768);
         casper.test.comment('Order categories');
 
-        casper.waitForSelector("#checklist-categories li:nth-child(2) .ng-binding:nth-child(1)",
+        casper.waitForSelector(".item-info h4 a",
             function success() {
                 var secondItemText1, secondItemText2;
                 // Read text in current 2nd item position
-                secondItemText1 = this.getHTML('#checklist-categories li:nth-child(2) a span:nth-child(1)');
+                secondItemText1 = this.getHTML('.item-info h4 a');
                 // Drag and drop
                 snapshotPage.call( this, 11);
-                moveMouse.call( casper, [450,331], [450,392], 20);
+                moveMouse.call( casper, [450,405], [450,475], 20);
                 // Read text in current 2nd item position
-                secondItemText2 = this.getHTML('#checklist-categories li:nth-child(2) a span:nth-child(1)');
+                secondItemText2 = this.getHTML('.item-info h4 a');
                 
                 this.wait(1000);
                 snapshotPage.call( this, 12);
