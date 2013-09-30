6b53bc6502352f26e120a145790311b919b1d4fb
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-09-30 16:24:49 +0200

    Added numattachmentsmixin tests

--- glynt/apps/todo/tests/__init__.py
+++ glynt/apps/todo/tests/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from glynt.apps.todo.tests.test_mixins import *
 from glynt.apps.todo.tests.test_feedbackrequests import *
 from glynt.apps.todo.tests.test_signals import *
 from glynt.apps.todo.tests.test_checklist_items_controls import *
