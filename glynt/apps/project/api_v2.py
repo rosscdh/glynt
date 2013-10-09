59e9f6a2a712bf76cce8f7f9b5f1a87ef8490054
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-10-09 13:04:28 +0200

    Added api_v2 endpoint and rest framework serializers class for project and discussion

---
+++ glynt/apps/project/api_v2.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+from django.shortcuts import get_object_or_404
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
+
+from threadedcomments.models import ThreadedComment
+
+from . import PROJECT_CONTENT_TYPE
+from .serializers import (ProjectSerializer, DiscussionSerializer, )
+from .models import Project
+
+
+class ProjectViewSet(ModelViewSet):
+    """
+    API endpoint that allows projects to be viewed or edited.
+    """
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = 'uuid'
+
+
+class DiscussionListView(ListAPIView):
+    """
+    Endpoint that shows discussion threads
+    django_comments & threaded_comments & fluent_comments
+    """
+    queryset = ThreadedComment.objects.all()
+    serializer_class = DiscussionSerializer
+
+    def get_queryset(self):
+        """
+        """
+        project_uuid = self.kwargs.get('uuid')
+        project = get_object_or_404(Project, uuid=project_uuid)
+
+        return self.queryset.filter(content_type=PROJECT_CONTENT_TYPE,
+                                    object_pk=project.pk)
+
+
+class DiscussionDetailView(RetrieveUpdateAPIView, DiscussionListView):
+    lookup_field = 'pk'
---
+++ glynt/apps/project/serializers.py
@@ -0,0 +1,86 @@
+# -*- coding: UTF-8 -*-
+from rest_framework import serializers
+
+from .models import Project
+from threadedcomments.models import ThreadedComment
+
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    customer = serializers.SerializerMethodField('get_customer')
+    company = serializers.SerializerMethodField('get_company')
+    transactions = serializers.SerializerMethodField('get_transactions')
+    lawyers = serializers.SerializerMethodField('get_lawyers')
+
+    status = serializers.SerializerMethodField('get_status')
+
+    class Meta:
+        model = Project
+        queryset = Project.objects.prefetch_related('customer', 'customer__user', 'company', 'transactions', 'lawyers', 'lawyers__user').all()
+        fields = ('uuid', 'customer', 'company',
+                  'transactions', 'lawyers',
+                  'status',)
+
+    def get_customer(self, obj):
+        user = obj.customer.user
+        return {
+            'pk': obj.pk,
+            'user_pk': user.pk,
+            'full_name': user.get_full_name(),
+            'photo': user.profile.get_mugshot_url(),
+        }
+
+    def get_company(self, obj):
+        return {
+            'pk': obj.company.pk,
+            'name': obj.data.get('company_name'),
+        }
+
+    def get_lawyers(self, obj):
+        return [{
+                    'pk': lawyer.pk,
+                    'user_pk': lawyer.user.pk,
+                    'full_name': lawyer.user.get_full_name(),
+                    'photo': lawyer.profile_photo,
+                    'url': lawyer.get_absolute_url(),
+                } for lawyer in obj.lawyers.all()]
+
+    def get_transactions(self, obj):
+        return [{'type': t.slug, 'name': t.title} for t in obj.transactions.all()]
+
+    def get_status(self, obj):
+        return {
+            'id': obj.status,
+            'name': obj.display_status,
+        }
+
+
+class DiscussionSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.SerializerMethodField('get_user')
+    timestamp = serializers.SerializerMethodField('get_timestamp')
+    comment_info =  serializers.SerializerMethodField('get_comment_tree')
+
+    class Meta:
+        model = ThreadedComment
+        fields = ('title', 'comment', 'user', 'user_url',
+                  'timestamp', 'comment_info')
+
+    def get_user(self, obj):
+        user = obj.user
+        return {
+            'pk': obj.pk,
+            'user_pk': user.pk,
+            'full_name': obj.user_name,
+            'photo': user.profile.get_mugshot_url(),
+        }
+    def get_timestamp(self, obj):
+        return obj.submit_date.strftime('%s')
+
+    def get_comment_tree(self, obj):
+        return {
+            'pk': obj.id,
+            'is_removed': obj.is_removed,
+            'tree_path': obj.tree_path,
+            'last_child': obj.last_child,
+            'is_public': obj.is_public
+        }
+        
