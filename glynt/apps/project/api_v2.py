dcf30a9986d97af4a05384316afb3d8a0c5436a6
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-10-10 16:10:51 +0200

    Added process for adding and saving team members

--- glynt/apps/project/api_v2.py
+++ glynt/apps/project/api_v2.py
@@ -1,14 +1,19 @@
 # -*- coding: UTF-8 -*-
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveUpdateDestroyAPIView
 
 from threadedcomments.models import ThreadedComment
 
 from . import PROJECT_CONTENT_TYPE
-from .serializers import (ProjectSerializer, DiscussionSerializer, )
-from .models import Project
+from .models import Project, ProjectLawyer
+from .serializers import (ProjectSerializer, TeamSerializer,
+                          DiscussionSerializer, )
 
 
 class ProjectViewSet(ModelViewSet):
@@ -20,27 +25,105 @@ class ProjectViewSet(ModelViewSet):
     lookup_field = 'uuid'
 
 
+class TeamListView(RetrieveUpdateAPIView):
+    """
+    Endpoint that shows team for a project
+    """
+    lawyers = []
+    customers = []
+    participants = []
+
+    queryset = Project.objects.all()
+    serializer_class = TeamSerializer
+    lookup_field = 'uuid'
+
+    def put(self, request, **kwargs):
+        raise PermissionDenied
+
+    def patch(self, request, **kwargs):
+        """
+        request.DATA should provide: [74,3,22] a list of User pks
+        we then calculate the user type and update the appropriate field
+        on the project
+        """
+        user_ids = request.DATA
+
+        if type(user_ids) not in [list] or len(user_ids) is 0:
+            raise ValidationError('You must PATCH a list into this view e.g. PATCH:[74,3,22]')
+        
+        self.project = self.get_object()
+
+        for u in User.objects.filter(pk__in=user_ids):
+            self.set_participant(user=u)
+
+        self.save_all()
+
+        serializer = self.get_serializer()
+
+        return Response(data=serializer.get_team(obj=self.project), status=202)
+
+    def set_lawyer(self, lawyer_profile):
+        self.lawyers.append(lawyer_profile)
+
+    def set_customer(self, customer_profile):
+        self.customers.append(customer_profile)
+
+    def set_participant(self, user):
+        self.participants.append(user)
+
+        if user.profile.is_lawyer is True:
+            self.set_lawyer(lawyer_profile=user.lawyer_profile)
+
+        elif user.profile.is_customer is True:
+            self.set_customer(customer_profile=user.customer_profile)
+
+    def save_all(self):
+        """
+        save all our bits
+        """
+        self.save_participants()
+        self.save_lawyers()
+        self.save_customer()
+
+    def save_lawyers(self):
+        if len(self.lawyers) > 0:
+            project = self.project
+
+            # get the current lawyers
+            project_lawyers = project.lawyers.all()
+
+            # remove those no longer present
+            for lawyer in project_lawyers:
+                if lawyer not in self.lawyers:
+                    ProjectLawyer.objects.get(project=project, lawyer=lawyer).delete()
+
+            # add new ones
+            for lawyer in self.lawyers:
+                if lawyer not in project_lawyers:
+                    #project.lawyers.add(lawyer) # CANT USE ADD due to custom through table
+                    # ProjectLawyer.objects.get_or_create(project=project, lawyer=lawyer, status=ProjectLawyer.LAWYER_STATUS.potential)  # removed temporarily until we talk about status
+                    ProjectLawyer.objects.get_or_create(project=project, lawyer=lawyer, status=ProjectLawyer.LAWYER_STATUS.assigned)
+
+    def save_customer(self):
+        """
+        customer never changes
+        """
+        pass
+
+    def save_participants(self):
+        """
+        save the participants
+        this is important as its participants that form
+        the notification chain
+        """
+        pass
+
+
+
 class DiscussionListView(ListCreateAPIView):
     """
     Endpoint that shows discussion threads
     django_comments & threaded_comments & fluent_comments
-    """
-    queryset = ThreadedComment.objects.all()
-    serializer_class = DiscussionSerializer
-
-    def get_queryset(self):
-        """
-        """
-        project_uuid = self.kwargs.get('uuid')
-        project = get_object_or_404(Project, uuid=project_uuid)
-
-        return self.queryset.filter(content_type=PROJECT_CONTENT_TYPE,
-                                    object_pk=project.pk)
-
-
-class TeamListView(ListCreateAPIView, RetrieveUpdateAPIView):
-    """
-    Endpoint that shows team for a project
     """
     queryset = ThreadedComment.objects.all()
     serializer_class = DiscussionSerializer
