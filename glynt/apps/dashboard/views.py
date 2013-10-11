c44b7391edff664973e382c2f2c6b6f4f02e799c
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-10-11 10:47:37 +0200

    Added RulezMixin to the dashboard view

--- glynt/apps/dashboard/views.py
+++ glynt/apps/dashboard/views.py
@@ -5,13 +5,14 @@ from django.shortcuts import get_object_or_404
 
 from bunch import Bunch
 
+from glynt.apps.rulez import RulezMixin
 from glynt.apps.project.models import Project
 from glynt.apps.project.bunches import ProjectIntakeFormIsCompleteBunch
 from glynt.apps.project import PROJECT_LAWYER_STATUS
 from glynt.apps.todo.views import ToDoCountMixin
 
 
-class DashboardView(ToDoCountMixin, TemplateView):
+class DashboardView(RulezMixin, ToDoCountMixin, TemplateView):
     """
     @TODO clean this mess up
     This view is used by both the customer and the lawyer (eek)
@@ -111,5 +112,7 @@ class DashboardView(ToDoCountMixin, TemplateView):
             'PROJECT_LAWYER_STATUS': PROJECT_LAWYER_STATUS,
             'projects': self.request.projects,
         })
+
+        self.can_read(project, user=self.request.user)
 
         return kwargs
