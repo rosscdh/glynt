9afce4ff7fe489badd797596c64027e6bcf3b278
Lee <lee@kudosknowledge.com>
2013-10-14 19:25:32 +1000

    Added tests for manage team dialog

--- glynt/apps/project/tests/lawyer_team_management.py
+++ glynt/apps/project/tests/lawyer_team_management.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 
 from glynt.casper import BaseLawyerCustomerProjectCaseMixin
 from glynt.apps.transact.models import Transaction
+from model_mommy import mommy
 
 import os
 
@@ -15,6 +16,7 @@ class LawyerProjectTeamManagementTest(BaseLawyerCustomerProjectCaseMixin):
         Test the team management interface
         """
         self.client.login(username=self.lawyer_user.username, password=self.password)
+        new_user = mommy.make('auth.User', username='linda-russo', first_name='Linda', last_name='Russo', email='lindajrusso@dayrep.com')
 
         url = reverse('dashboard:project', kwargs={'uuid': self.project.uuid})
         self.assertTrue(self.load_casper_file(js_file='lawyer-team-management.js', test_label='Test the Lawyer can manage the project team', url=url))
