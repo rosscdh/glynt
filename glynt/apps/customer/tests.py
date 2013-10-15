433fc6611acd70f0e717dcef5b399cc390f9e48f
Ross Crawford <sendrossemail@gmail.com>
2013-10-15 05:27:26 +0200

    Added test for customer models

--- glynt/apps/customer/tests.py
+++ glynt/apps/customer/tests.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
+from django.db.models.query import QuerySet
 
-from glynt.casper import BaseLawyerCustomerProjectCaseMixin, PyQueryMixin
+from glynt.casper import BaseLawyerCustomerProjectCaseMixin
 
 
-class CustomerProfileSetupFormTest(BaseLawyerCustomerProjectCaseMixin, PyQueryMixin):
+class CustomerModelTest(BaseLawyerCustomerProjectCaseMixin):
+    def test_get_absolute_url(self):
+        self.assertEqual('/customers/customer/', self.customer.get_absolute_url())
+
+    def test_full_name(self):
+        self.assertEqual(u' ', self.customer.full_name)
+
+    def test_profile_photo(self):
+        self.assertEqual('/static/img/default_avatar.png', self.customer.profile_photo)
+
+    def test_phone(self):
+        self.assertEqual('', self.customer.phone)
+
+    def test_companies(self):
+        self.assertEqual(QuerySet, type(self.customer.companies))
+        self.assertEqual(1, len(self.customer.companies))
+
+    def test_primary_company(self):
+        self.assertEqual(self.company, self.customer.primary_company)
+
+
+class CustomerProfileSetupFormTest(BaseLawyerCustomerProjectCaseMixin):
     def test(self):
         url = reverse('customer:setup_profile')
 
