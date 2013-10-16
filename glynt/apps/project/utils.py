83afe51aebc345be53350ee6d03d14c6de72c078
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-10-16 13:12:43 +0200

    turned constants into functions

--- glynt/apps/project/utils.py
+++ glynt/apps/project/utils.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
 
-PROJECT_CONTENT_TYPE = ContentType.objects.get(app_label="project", model="project")
-PROJECTLAWYER_CONTENT_TYPE = ContentType.objects.get(app_label="project", model="projectlawyer")
+
+def PROJECT_CONTENT_TYPE():
+    return ContentType.objects.get(app_label="project", model="project")
+
+def PROJECTLAWYER_CONTENT_TYPE():
+    return ContentType.objects.get(app_label="project", model="projectlawyer")
