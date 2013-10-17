77b35822fa2c586f863179adf8f8353fd1b03724
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-10-17 16:22:32 +0200

    Bade default accessible by dot notation

--- glynt/apps/project/utils.py
+++ glynt/apps/project/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
-
+from bunch import Bunch
 
 try:
     _PROJECT_CONTENT_TYPE = ContentType.objects.get(app_label="project", model="project")
@@ -8,5 +8,5 @@ try:
 except:
     # for when there are no contenttype obejcts yet
     # take a wile guess based on local
-    _PROJECT_CONTENT_TYPE = 14
-    _PROJECTLAWYER_CONTENT_TYPE = 15
+    _PROJECT_CONTENT_TYPE = Bunch(pk=14, app_label="project", model="project")
+    _PROJECTLAWYER_CONTENT_TYPE = Bunch(pk=15, app_label="project", model="projectlawyer")
