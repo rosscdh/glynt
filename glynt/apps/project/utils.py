f023ce11d2990fa5e1be528ed4e7a265eaa17213
Ross Crawford <sendrossemail@gmail.com>
2013-10-18 09:05:27 +0200

    wokring on patch for content object

--- glynt/apps/project/utils.py
+++ glynt/apps/project/utils.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
-from bunch import Bunch
 
 try:
     _PROJECT_CONTENT_TYPE = ContentType.objects.get(app_label="project", model="project")
@@ -8,5 +7,9 @@ try:
 except:
     # for when there are no contenttype obejcts yet
     # take a wile guess based on local
-    _PROJECT_CONTENT_TYPE = Bunch(pk=14, app_label="project", model="project")
-    _PROJECTLAWYER_CONTENT_TYPE = Bunch(pk=15, app_label="project", model="projectlawyer")
+    # @BUSINESS_RULE these pk values are based on the preview and live values
+    # if you need it for dev look up your local dev shell_plus
+    # >>> ContentType.objects.filter(app_label="project", model="project")[0].pk
+    # >>> ContentType.objects.filter(app_label="project", model="projectlawyer")[0].pk
+    _PROJECT_CONTENT_TYPE = ContentType(pk=15, app_label="project", model="project")
+    _PROJECTLAWYER_CONTENT_TYPE = ContentType(pk=16, app_label="project", model="projectlawyer")
