c4d355bce23bb4d93f6a2ee9f0c19ac630cb12d7
Ross Crawford <sendrossemail@gmail.com>
2013-10-18 09:06:33 +0200

    wokring on patch for content object

--- glynt/apps/project/utils.py
+++ glynt/apps/project/utils.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
 
+import logging
+logger = logging.getLogger('django.request')
+
+
 try:
     _PROJECT_CONTENT_TYPE = ContentType.objects.get(app_label="project", model="project")
     _PROJECTLAWYER_CONTENT_TYPE = ContentType.objects.get(app_label="project", model="projectlawyer")
@@ -11,5 +15,6 @@ except:
     # if you need it for dev look up your local dev shell_plus
     # >>> ContentType.objects.filter(app_label="project", model="project")[0].pk
     # >>> ContentType.objects.filter(app_label="project", model="projectlawyer")[0].pk
+    logger.critical('ContentType is being set manually in project.utils should not happen for web requests, but can for cli events')
     _PROJECT_CONTENT_TYPE = ContentType(pk=15, app_label="project", model="project")
     _PROJECTLAWYER_CONTENT_TYPE = ContentType(pk=16, app_label="project", model="projectlawyer")
