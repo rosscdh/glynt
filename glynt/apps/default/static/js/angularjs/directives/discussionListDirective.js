9cee3994d7497b1f3aeadb06dcb1020565661114
Lee <lee@kudosknowledge.com>
2013-10-23 20:39:36 +1000

    Adjust +new button position, make entire discussion item clickable

--- glynt/apps/default/static/js/angularjs/directives/discussionListDirective.js
+++ glynt/apps/default/static/js/angularjs/directives/discussionListDirective.js
@@ -77,7 +77,7 @@ angular.module('lawpal').directive('discussionList', [ 'lawPalService', 'toaster
 			 * Request display of a specific discussion
 			 * @param  {Object} discussion Discussion item to display
 			 */
-			$scope.displayDiscussion = function( discussion ) {
+			$scope.displayDiscussion = function( $event, discussion ) {
 				discussionItemService.show( discussion );
 			};
 
@@ -92,7 +92,8 @@ angular.module('lawpal').directive('discussionList', [ 'lawPalService', 'toaster
 			 * Request a reply
 			 * @param  {Object} discussion Parent discussion object
 			 */
-			$scope.reply = function( discussion ){
+			$scope.reply = function( $event, discussion ){
+				$event.stopPropagation();
 				discussionItemService.reply( discussion );
 			};
 
@@ -108,6 +109,11 @@ angular.module('lawpal').directive('discussionList', [ 'lawPalService', 'toaster
 				$scope.generateWorkingDiscussionData(  /*message.parent_id, response.id*/ );
 			});
 
+			/**
+			 * Used when responding to discussions, updates last child on existing array elements
+			 * @param {Number} parentId Database ID of parent discussion
+			 * @param {Object} newItem  Typically response from API
+			 */
 			var addLastChild = function( parentId, newItem ) {
 				for(var i=0;i< $scope.discussions.length;i++ ) {
 					if( $scope.discussions[i].id===parentId) {
@@ -115,25 +121,6 @@ angular.module('lawpal').directive('discussionList', [ 'lawPalService', 'toaster
 					}
 				}
 			};
-
-			/**
-			 * Given and Object attempt to find it quickly, uses object keys for speed. If an object is not passed through then the working list is generated.
-			 * Please note that working is not in $scope. This is to avoid angular watching for changes and reducing page speed
-			 * @param  {Array/String} obj Array of discussion items or a look up string
-			 * @return {Object}	 Discussion item
-			 */
-			/*
-			function discussionLookup( obj ) {
-				if(angular.isArray(obj)) {
-					for (var i=0;i<obj.length;i++) {
-						working.dicussions[obj[i].id] = obj[i];
-					}
-				}
-				else {
-					return working.dicussions[obj]||null;
-				}
-			}
-			*/
 		}]
 	};
 }]);
@@ -149,13 +136,13 @@ angular.module('lawpal').run(["$templateCache", function($templateCache) {
 		'<span ng-show="working.loading" class="text-muted"><i class="icon icon-spinner icon-spin"></i> Loading discussions</span>\n'+
 		'<p ng-show="working.discussions.length==0" class="text-muted"><button class="btn btn-link" ng-click="new(null)">Start a discussion</button></p>\n'+
 		'<table class="table table-striped">\n'+
-		'	<tr ng-repeat="discussion in working.discussions  | orderBy:\'latest.id\':true" class="byme-{[{byMe(discussion.latest.meta.user.pk)}]}">\n'+
-		'		<td class="status-column">\n'+
+		'	<tr ng-repeat="discussion in working.discussions  | orderBy:\'latest.id\':true" class="byme-{[{byMe(discussion.latest.meta.user.pk)}]}"  ng-click="displayDiscussion( $event, discussion)">\n'+
+		'		<td class="status-column clickable">\n'+
 		'			<i class="icon icon-comment nest">\n'+
 		'				<small class="nested text-primary" ng-bind="discussion.original.count">1</small>\n'+
 		'			</i>\n'+
 		'		</td>\n'+
-		'		<td class="vcard-column">\n'+
+		'		<td class="vcard-column clickable">\n'+
 		'			<div class="fn fn-large vcard" user-mini-widget user="discussion.latest.meta.user" data-show-props="photo"></div>\n'+
 		'			<div class="time text-muted">\n'+
 		/*'				<i class="icon icon-time"></i>\n'+*/
@@ -163,16 +150,16 @@ angular.module('lawpal').run(["$templateCache", function($templateCache) {
 		'			</div>\n'+
 		'		</td>\n'+
 		'		<td class="comment-column">\n'+
-		'			<div class="comment clickable" ng-click="displayDiscussion(discussion)">\n'+
+		'			<div class="comment clickable">\n'+
 		'			    <p ng-show="discussion.original.title"><strong ng-bind="discussion.original.title | characters:50"></strong></p>\n'+
 		'			    <p ng-bind="discussion.latest.comment | characters:200"></p>\n'+
 		'			</div>\n'+
-		'			<div><button type="button" class="btn btn-link btn-small pull-right" tooltip="Respond now" ng-click="reply(discussion.original)">\n'+
+		'			<div><button type="button" class="btn btn-link btn-small pull-right" tooltip="Respond now" ng-click="reply( $event, discussion.original)">\n'+
 		'				<i class="icon icon-reply"></i> Respond\n'+
 		'			</button></div>\n'+
 		'		</td>\n'+
 		'		<td class="more-column clickable">\n'+
-		'			<i class="icon icon-chevron-right text-muted" ng-click="displayDiscussion(discussion)"></i>\n'+
+		'			<i class="icon icon-chevron-right text-muted"></i>\n'+
 		'		</td>\n'+
 		'	</tr>\n'+
 		'</table>\n'
