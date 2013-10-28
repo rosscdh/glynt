3b1d6a226966f90b997b7d704f9858a8a781c263
Lee <lee@kudosknowledge.com>
2013-10-28 21:11:17 +1000

    Override default angular-ui bootstrap modal dialog wrapper, to remove slide down animation

--- glynt/apps/default/static/js/angularjs/lawpal.app.js
+++ glynt/apps/default/static/js/angularjs/lawpal.app.js
@@ -51,5 +51,7 @@ lawPalApp.filter('openStatus', function () {
 	};
 });
 
-
-
+angular.module("template/modal/window.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("template/modal/window.html",
+    "<div class=\"modal {{ windowClass }}\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1050 + index*10}\" ng-transclude></div>");
+}]);
