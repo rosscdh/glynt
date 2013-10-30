bcde0199d8cd64f50d2bc4e166fe65a9a8ba8099
Jamie Connolly <jamie@lawpal.com>
2013-10-30 15:35:51 +0000

    Actually send the flow to the transaction build stage

--- glynt/apps/default/static/temp/js/main.js
+++ glynt/apps/default/static/temp/js/main.js
@@ -1,43 +1,48 @@
+// set up the blank defaults
+window['fixed-fees']            = [];
+window['intake-forms']          = [];
+window['needs-qualification']   = [];
+window['selected-transactions'] = {};
+window['services']              = [];
+
 // SELECTION SCENE EVENTS
 // ======================
 $(document).on('click', '[data-scene="selection"] .btn-next-step', function(e) {
     var $this  = $(e.target);
-    var $slide = $(this).closest('section.slide');
+    var $slide = $this.closest('section.slide');
 
-    // set up the blank defaults
-    window['fixed-fees']            = [];
-    window['intake-forms']          = [];
-    window['needs-qualification']   = [];
-    window['selected-transactions'] = [];
-    window['services']              = [];
+    var fixedFees          = [];
+    var intakeForms        = [];
+    var needsQualification = [];
+    var services           = [];
 
     e.preventDefault();
 
     $slide.find('[type="checkbox"]').each(function() {
-        var $element  = $(this);
+        var $element = $(this);
 
         if ($element.is(':checked')) {
-            window['services'].push($element.attr('name'));
+            services.push($element.attr('name'));
 
             if ($element.data('fixed-fee')) {
-                window['fixed-fees'].push($element.attr('name'));
+                fixedFees.push($element.attr('name'));
             }
 
             if ($element.data('intake-form')) {
-                window['intake-forms'].push($element.attr('name'));
+                intakeForms.push($element.attr('name'));
             }
 
             if ($element.data('needs-qualification')) {
-                window['needs-qualification'].push($element.attr('name'));
+                needsQualification.push($element.attr('name'));
             }
         }
     });
 
     // make sure we only save things once
-    window['fixed-fees'] = $.unique(window['fixed-fees']);
-    window['intake-forms'] = $.unique(window['intake-forms']);
-    window['needs-qualification'] = $.unique(window['needs-qualification']);
-    window['services'] = $.unique(window['services']);
+    window['fixed-fees'] = $.unique(fixedFees);
+    window['intake-forms'] = $.unique(intakeForms);
+    window['needs-qualification'] = $.unique(needsQualification);
+    window['services'] = $.unique(services);
 
     // only go forward if more than one has been selected
     if (window['services'].length > 0) {
@@ -49,7 +54,6 @@ $(document).on('click', '[data-scene="selection"] .btn-next-step', function(e) {
 // ==========================
 $(document).on('click', '[data-scene="qualification"] .btn-next-step', function(e) {
     var $this  = $(e.target);
-    var $slide = $this.closest('section.slide');
 
     e.preventDefault();
 
@@ -61,14 +65,18 @@ $(document).on('click', '[data-scene="qualification"] .btn-next-step', function(
 // ======================
 $(document).on('click', '[data-scene="fixed-fee"] .btn-next-step', function(e) {
     var $this  = $(e.target);
+    var $node = $this.closest('.transaction-choice');
     var $slide = $this.closest('section.slide');
+
+    var transaction = $node.attr('data-transaction');
+    var transactionType = $slide.attr('data-transaction-type');
 
     e.preventDefault();
 
-    // has the form been filled out properly?
-        nextStep();
-});
+    window['selected-transactions'][transactionType] = transaction;
 
+    nextStep();
+});
 
 var nextStep = function() {
     // are there any qualification steps needed?
@@ -82,4 +90,16 @@ var nextStep = function() {
         var nextStep = window['fixed-fees'].shift();
         return window.location.hash = "#fixed-fee-" + nextStep;
     }
+
+    // no more steps to show
+    var $form  = $('form#transaction-form');
+    var $input = $form.find('#id_transaction_type');
+
+    var selectedTransactions = [];
+    $.each(window['selected-transactions'], function(key, value) {
+        selectedTransactions.push(value);
+    });
+    $input.attr('value', selectedTransactions);
+
+    $form.submit();
 }
