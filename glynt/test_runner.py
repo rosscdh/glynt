be87c843ab37ed27a148384f9e53438520db212e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-10-31 12:45:55 +0100

    Modified runner to collectsatic and remove after test

--- glynt/test_runner.py
+++ glynt/test_runner.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from django.core.management import call_command
 from django.test.simple import DjangoTestSuiteRunner
 
 from colortools.test import ColorDjangoTestSuiteRunner
+
+import shutil
 
 
 class GlyntAppTestRunner(ColorDjangoTestSuiteRunner, DjangoTestSuiteRunner):
@@ -17,3 +20,11 @@ class GlyntAppTestRunner(ColorDjangoTestSuiteRunner, DjangoTestSuiteRunner):
             test_labels = tuple(test_labels)
 
         return super(GlyntAppTestRunner, self).build_suite(test_labels, *args, **kwargs)
+
+    def setup_test_environment(self, **kwargs):
+        call_command('collectstatic', interactive=False)  # collect static so our casper tests break less
+        super(GlyntAppTestRunner, self).setup_test_environment(**kwargs)
+
+    def teardown_test_environment(self, **kwargs):
+        shutil.rmtree(settings.STATIC_ROOT)  # delete the static folder
+        super(GlyntAppTestRunner, self).teardown_test_environment(**kwargs)
