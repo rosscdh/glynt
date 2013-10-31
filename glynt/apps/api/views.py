2df4b52dfce7aec6ac2637360b77b478b0ebf1e8
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-10-31 15:57:36 +0100

    removed reliance on the v2 api injection hack

--- glynt/apps/api/views.py
+++ glynt/apps/api/views.py
@@ -3,41 +3,42 @@ from rest_framework.test import APIClient
 import json
 
 
-class v2ApiClientMixin(object):
-    """
-    Mixin to allow us to replicate a query to the API
-    which allows us to prepopulate the view with JSON data thats
-    exactly the same as would be gotten by querying the API
-    """
-    api_client = None
-    api_uri = None
-    api_resp = None
+# class v2ApiClientMixin(object):
+#     """
+#     Mixin to allow us to replicate a query to the API
+#     which allows us to prepopulate the view with JSON data thats
+#     exactly the same as would be gotten by querying the API
+#     """
+#     api_client = None
+#     api_uri = None
+#     api_resp = None
 
-    def __init__(self, *args, **kwargs):
-        """
-        set the api_client var
-        """
-        self.api_client = APIClient()
-        super(v2ApiClientMixin, self).__init__(*args, **kwargs)
+#     def dispatch(self, request, *args, **kwargs):
+#         """
+#         set the api_client var
+#         """
+#         self.api_client = APIClient()
+#         return super(v2ApiClientMixin, self).dispatch(request=request, *args, **kwargs)
 
-    def api_query(self, request, url=None, method='get', **kwargs):
-        url = url if url is not None else self.api_uri
+#     def api_query(self, request, url=None, method='get', **kwargs):
+#         url = url if url is not None else self.api_uri
 
-        if 'format' not in kwargs:
-            kwargs.update({
-                'format': 'json'
-            })
+#         if 'format' not in kwargs:
+#             kwargs.update({
+#                 'format': 'json'
+#             })
 
-        assert url is not None, "You must pass in a url=':path' or specify api_uri on this view"
+#         assert url is not None, "You must pass in a url=':path' or specify api_uri on this view"
 
-        user = request.user
-        self.api_client.force_authenticate(user=user)  # authenticate as current user
-        # get the method to call (get,list,put,patch,delete)
-        method = getattr(self.api_client, method)
+#         user = request.user
+#         self.api_client.force_authenticate(user=user)  # authenticate as current user
 
-        self.api_resp = method(url, **kwargs)
+#         # get the method to call (get,list,put,patch,delete)
+#         method = getattr(self.api_client, method)
 
-        return self
+#         self.api_resp = method(url, **kwargs)
 
-    def api_resp_as_json(self):
-        return json.loads(self.api_resp.content)
+#         return self
+
+#     def api_resp_as_json(self):
+#         return json.loads(self.api_resp.content)
--- glynt/apps/dashboard/views.py
+++ glynt/apps/dashboard/views.py
@@ -9,12 +9,12 @@ from glynt.apps.rulez import RulezMixin
 from glynt.apps.project.models import Project
 from glynt.apps.project.bunches import ProjectIntakeFormIsCompleteBunch
 from glynt.apps.project import PROJECT_LAWYER_STATUS
+from glynt.apps.project.api_v2 import ProjectViewSet
 
 from glynt.apps.todo.views import ToDoCountMixin
-from glynt.apps.api.views import v2ApiClientMixin
 
 
-class DashboardView(RulezMixin, ToDoCountMixin, v2ApiClientMixin, TemplateView):
+class DashboardView(RulezMixin, ToDoCountMixin, TemplateView):
     """
     @TODO clean this mess up
     This view is used by both the customer and the lawyer (eek)
@@ -131,10 +131,6 @@ class DashboardView(RulezMixin, ToDoCountMixin, v2ApiClientMixin, TemplateView):
         kwargs.update({
             'PROJECT_LAWYER_STATUS': PROJECT_LAWYER_STATUS,
             'projects': self.get_projects(),
-            'json': {
-                'projects': self.api_query(request=self.request, url='/api/v2/project/').api_resp_as_json(),
-                'discussions': [self.api_query(request=self.request, url='/api/v2/project/{uuid}/discussion/'.format(uuid=p.uuid)).api_resp_as_json() for p in self.get_projects()],
-            }
         })
 
         return kwargs
