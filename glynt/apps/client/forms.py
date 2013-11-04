e182dc4d6f47319c4b97f8d9a768ab16d95ab2a0
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-04 12:45:21 +0100

    made t&c a link and open in a new window .. made company not a required field

--- glynt/apps/client/forms.py
+++ glynt/apps/client/forms.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.core import exceptions
-
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse_lazy
 
 from parsley.decorators import parsleyfy
 
@@ -21,10 +21,10 @@ class ConfirmLoginDetailsForm(forms.ModelForm):
     """
     first_name = forms.CharField(max_length=24, widget=forms.TextInput(attrs={'placeholder': 'John'}))
     last_name = forms.CharField(max_length=24, widget=forms.TextInput(attrs={'placeholder': 'Doemann'}))
-    company_name = forms.CharField(label="Company", help_text='', widget=forms.TextInput(attrs={'placeholder': 'Acme Inc'}))
+    company_name = forms.CharField(label="Company", required=False, help_text='', widget=forms.TextInput(attrs={'placeholder': 'Acme Inc'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'john.doemann@example.com'}))
     phone = forms.CharField(label="Phone number", widget=forms.TextInput(attrs={'data-type': 'phone', 'autocomplete': 'off'}))
-    agree_tandc = forms.BooleanField(label='I agree to the Terms &amp; Conditions', help_text='')
+    agree_tandc = forms.BooleanField(label='I agree to the <a target="_BLANK" href="{url}">Terms &amp; Conditions</a>', help_text='')
 
     class Meta:
         model = User
@@ -35,6 +35,7 @@ class ConfirmLoginDetailsForm(forms.ModelForm):
         self.request = kwargs.pop('request', None)
         self.user = self.request.user
         super(ConfirmLoginDetailsForm, self).__init__(*args, **kwargs)
+        self.fields['agree_tandc'].label = self.fields['agree_tandc'].label.format(url=reverse_lazy('public:terms'))
 
     def clean_email(self):
         email = self.cleaned_data['email']
