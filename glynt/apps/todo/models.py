6529e999f72b1e4b99eafae4d92b4b035e66eb67
Ross Crawford <sendrossemail@gmail.com>
2013-11-05 22:07:52 +0100

    moved signals import into modls! has to be here for production

--- glynt/apps/todo/models.py
+++ glynt/apps/todo/models.py
@@ -196,3 +196,16 @@ class FeedbackRequest(models.Model):
         except IndexError:
             return {}
 
+
+"""
+import signals
+"""
+from .signals import (on_attachment_created,
+                      on_attachment_deleted,
+                      on_comment_created,
+                      feedbackrequest_created,
+                      feedbackrequest_status_change,
+                      projectlawyer_assigned,
+                      projectlawyer_deleted,
+                      todo_item_status_change,
+                      on_action_created,)
