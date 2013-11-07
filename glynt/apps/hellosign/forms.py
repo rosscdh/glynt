2aa71ab70597ba88e1f6b65092388f4b17c7151e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-07 17:11:58 +0100

    Added forms

---
+++ glynt/apps/hellosign/forms.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+from parsley.decorators import parsleyfy
+
+from models import Signature
+
+
+@parsleyfy
+class SignatureForm(forms.ModelForm):
+    user = None
+    subject = forms.CharField(required=True)
+    message =  forms.CharField(required=True)
+
+    class Meta:
+        model = Signature
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(SignatureForm, self).__init__(*args, **kwargs)
+
+    def signatories(self):
+        return [('ross', 'ross@lawpal.com'),]
+
+    def documents(self):
+        return []
