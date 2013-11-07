a2b590a0e76da3ac407c1f7ce7225fa1ae462032
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-07 17:10:51 +0100

    added getters and setters for json data attribs

--- glynt/apps/hellosign/models.py
+++ glynt/apps/hellosign/models.py
@@ -10,6 +10,22 @@ class Signature(models.Model):
     requested_by = models.ForeignKey('auth.User')
     signatories = models.ManyToManyField('auth.User', related_name='signatories')
     project = models.ForeignKey('project.Project')
-    data = JSONField(default={})
-    signature_request_id = models.CharField(max_length=255, db_index=True)
+    data = JSONField(default={}, blank=True)
+    signature_request_id = models.CharField(max_length=255, blank=True, db_index=True)
     is_complete = models.BooleanField(default=False, db_index=True)
+
+    @property
+    def subject(self):
+        return self.data.get('subject', None)
+
+    @subject.setter
+    def subject(self, value):
+        self.data['subject'] = value
+
+    @property
+    def message(self):
+        return self.data.get('message', None)
+
+    @message.setter
+    def message(self, value):
+        self.data['message'] = value
