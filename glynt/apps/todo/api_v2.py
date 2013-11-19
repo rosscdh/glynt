bfa696cc00948b0bc76d4784ba8a9e0fe4310ce8
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-19 10:41:05 +0100

    Added todo discussion endpoint

--- glynt/apps/todo/api_v2.py
+++ glynt/apps/todo/api_v2.py
@@ -4,7 +4,10 @@ from django.core.exceptions import PermissionDenied
 
 from rest_framework.viewsets import ModelViewSet
 
+from threadedcomments.models import ThreadedComment
+
 from glynt.apps.project.models import Project
+from glynt.apps.project.serializers import (DiscussionThreadSerializer,)
 
 from .serializers import AttachmentSerializer
 from .models import ToDo, Attachment
@@ -34,3 +37,19 @@ class AttachmentViewSet(ModelViewSet):
 
     def destroy(self, request, pk=None):
         raise PermissionDenied
+
+
+class ToDoDiscussionDetailView(ModelViewSet):
+    queryset = ThreadedComment.objects.select_related('user', 'tagged_items__tag').all().order_by('-id')
+    serializer_class = DiscussionThreadSerializer
+    lookup_field = 'pk'
+
+    def get_queryset(self):
+        parent_pk = self.kwargs.get('parent_pk')
+        get_object_or_404(Project, uuid=self.kwargs.get('uuid'))  # ensure that we have the project
+        get_object_or_404(ToDo, slug=self.kwargs.get('slug'))  # ensure we have the slug
+        #
+        # @BUSINESS RULE: /discussion/:pk/ should return the parent
+        # as well as a the children as a "thread": []
+        #
+        return self.queryset.filter(pk=parent_pk)
