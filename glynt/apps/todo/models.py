69816f542176da19e69a64608f97a659614c515d
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-09 10:24:04 +0100

    Moved todo imports from models to __init__

--- glynt/apps/todo/models.py
+++ glynt/apps/todo/models.py
@@ -195,17 +195,3 @@ class FeedbackRequest(models.Model):
             return self.assigned_to.all()[0]
         except IndexError:
             return {}
-
-
-"""
-import signals
-"""
-from .signals import (on_attachment_created,
-                      on_attachment_deleted,
-                      on_comment_created,
-                      feedbackrequest_created,
-                      feedbackrequest_status_change,
-                      projectlawyer_assigned,
-                      projectlawyer_deleted,
-                      todo_item_status_change,
-                      on_action_created,)
